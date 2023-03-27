2eacd9c0bdda3608c9199a99ad8df691ac9b7935
DarkDev <darkdev_brand@riseup.net>
2023-03-28 00:48:31 +0300

    upd api

--- src/lib/entities/index.js
+++ src/lib/entities/index.js
@@ -1,4 +1,24 @@
 import CountdownTimer from './ui/CountdownTimer.svelte';
 import StepsList from './ui/StepsList.svelte';
+import Welcome from './ui/Welcome.svelte';
+import StepsBtns from './ui/StepsBtns.svelte';
 
-export { CountdownTimer, StepsList };
+import MapGenerator from './ui/welcomeScreen/MapGenerator.svelte';
+import MapSetup from './ui/welcomeScreen/MapSetup.svelte';
+import ExperimentsGenerator from './ui/welcomeScreen/ExperimentsGenerator.svelte';
+import ResourcesGenerator from './ui/welcomeScreen/ResourcesGenerator.svelte';
+import RolesSetup from './ui/welcomeScreen/RolesSetup.svelte';
+import LevelSecelect from './ui/welcomeScreen/LevelSecelect.svelte';
+
+export {
+	CountdownTimer,
+	StepsList,
+	Welcome,
+	StepsBtns,
+	MapGenerator,
+	MapSetup,
+	RolesSetup,
+	ExperimentsGenerator,
+	ResourcesGenerator,
+	LevelSecelect
+};
