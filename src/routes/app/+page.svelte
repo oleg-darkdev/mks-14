c1a1408bc32e01a8dc7b92f58567b6e84f9b8ad6
DarkDev <darkdev_brand@riseup.net>
2023-03-27 17:26:37 +0300

    basic logic of app

---
+++ src/routes/app/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+	import {
+		FooterApp,
+		StartScreen,
+		GameProgressScreen,
+		InstrustionScreen
+	} from '../../lib/widgets/';
+
+	let activeScreen = 'Rozpoczęcie';
+
+
+</script>
+
+<section class="flex h-screen flex-col items-center">
+	{#if activeScreen === 'Rozpoczęcie'}
+		<StartScreen  />
+	{:else if activeScreen === 'Instrukcja'}
+		<InstrustionScreen  />
+	{:else if activeScreen === 'Rozgrywka'}
+		<GameProgressScreen />
+	{/if}
+</section>
+<FooterApp bind:activeScreen />
