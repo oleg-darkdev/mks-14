6f69ce648fda79396e8115ab7bd3d04a157b3631
DarkDev <darkdev_brand@riseup.net>
2023-03-27 17:28:51 +0300

    alfa v. timer

---
+++ src/lib/entities/ui/CountdownTimer.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { tweened } from 'svelte/motion';
+	let original = 5 * 60; 
+	let timer = tweened(original);
+
+	setInterval(() => {
+		if ($timer > 0) $timer--;
+	}, 1000);
+
+	$: minutes = Math.floor($timer / 60);
+	$: minname = minutes > 1 ? 'mins' : 'min';
+	$: seconds = Math.floor($timer - minutes * 60);
+</script>
+
+<span class="font-mono countdown text-6xl">
+	<span style="--value:{seconds};" />
+</span>
