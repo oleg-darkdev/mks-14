6d17b4ee9a51f8b09db94e1f7cb9b6484e9a3444
DarkDev <darkdev_brand@riseup.net>
2023-03-28 00:48:51 +0300

    create base structure

--- src/lib/widgets/ui/StartScreen.svelte
+++ src/lib/widgets/ui/StartScreen.svelte
@@ -1,82 +1,82 @@
 <script>
-	import { LevelCard } from '../../shared';
-	import { StepsList } from '../../entities';
-
-	const levelsList = [
-		{
-			title: 'Easy',
-			desc: 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries.',
-			id: 1,
-			settings: {
-				timer: '',
-				color: '',
-				experiments: '',
-				resources: ''
-			}
-		},
-		{
-			title: 'Medium',
-			desc: 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries.',
-			id: 2,
-			settings: {
-				timer: '',
-				color: '',
-				experiments: '',
-				resources: ''
-			}
-		},
-		{
-			title: 'hard',
-			desc: 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries.',
-			id: 3,
-			settings: {
-				timer: '',
-				color: '',
-				experiments: '',
-				resources: ''
-			}
-		}
-	];
+	import {
+		StepsList,
+		Welcome,
+		StepsBtns,
+		ExperimentsGenerator,
+		ResourcesGenerator,
+		MapSetup,
+		MapGenerator,
+		RolesSetup,
+		LevelSecelect
+	} from '../../entities';
 
 	const gameSteps = [
 		{
-			title: 'Lorem ipsum is placeholder text',
+			title: 'Welcome L.O.R.',
 			desc: 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.',
 			img: ''
 		},
 		{
-			title: 'Lorem ipsum is placeholder text',
+			// игроки указывают количество участников - это повлияет на количество экспериментов и ресурсев
+			// Игроки должны выбрать себе 1 персонажа, отложить его карточку в колоду проиществий.
+			title: 'Wybór bohaterów',
 			desc: 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.',
 			img: ''
 		},
 		{
-			title: 'Lorem ipsum is placeholder text',
+			title: 'Сетап карты',
 			desc: 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.',
 			img: ''
 		},
 		{
-			title: 'Lorem ipsum is placeholder text',
+			title: 'Выберите уровень сложности',
+			desc: 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.',
+			img: ''
+		},
+		{
+			title: 'Количество экспериментов',
+			desc: 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.',
+			img: ''
+		},
+		{
+			title: 'Положите количество ресурсев на МКС',
 			desc: 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.',
 			img: ''
 		}
 	];
 
-	// save on store
-	function selectMode(levelInfo) {
-		selectedMode = levelInfo;
-		console.log(levelInfo);
-	}
-
-	export let selectedMode;
+	let step = 1;
 </script>
 
-<StepsList steps={gameSteps} />
-
-<div class="flex h-full flex-col justify-center">
-	<h2 class="text-center text-2xl text-white">Hejka</h2>
-	<div class="flex flex-row flex-wrap items-center ">
-		{#each levelsList as levelInfo}
-			<LevelCard on:click={() => selectMode(levelInfo)} {levelInfo} />
-		{/each}
-	</div>
+<div class="flex h-full flex-col items-center justify-center pt-40 pb-10">
+	{#if step == 1}
+		<Welcome
+			title="Lorem ipsum is placeholder"
+			desc="Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups."
+		>
+			<StepsBtns bind:step />
+		</Welcome>
+	{:else if step == 2}
+		<StepsList steps={gameSteps}>
+			<StepsBtns bind:step />
+		</StepsList>
+	{:else if step == 3}
+		<RolesSetup bind:step />
+	{:else if step == 4}
+		<MapSetup>
+			<StepsBtns bind:step />
+		</MapSetup>
+		<MapGenerator />
+	{:else if step == 5}
+		<LevelSecelect bind:step  />
+	{:else if step == 6}
+		<ExperimentsGenerator>
+			<StepsBtns bind:step />
+		</ExperimentsGenerator>
+	{:else if step == 7}
+		<ResourcesGenerator>
+			<StepsBtns bind:step />
+		</ResourcesGenerator>
+	{/if}
 </div>
