2dc2509b16377a776770eb0207b99a8c2828c3f2
DarkDev <darkdev_brand@riseup.net>
2023-03-28 00:49:00 +0300

    refactoring

--- src/lib/widgets/ui/GameProgressScreen.svelte
+++ src/lib/widgets/ui/GameProgressScreen.svelte
@@ -1,9 +1,16 @@
 <script>
-	import { CountdownTimer } from '../../entities';
+	import { CountdownTimer, Welcome, StepsBtns } from '../../entities';
+
+	let step = 1;
 </script>
 
 <div class="flex h-full flex-col justify-center">
-	<h2 class="text-center text-2xl text-white">Hejka</h2>
+	<Welcome
+		title="Lorem ipsum is placeholder"
+		desc="Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups."
+	>
+		<StepsBtns bind:step />
+	</Welcome>
 	<div class="flex flex-row flex-wrap items-center ">
 		<!-- {#each levelsList as levelInfo}
 			<LevelCard on:click={() => selectMode(levelInfo)} {levelInfo} />
