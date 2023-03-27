9117d5eebdc2842c7551f312f076db477c404d63
DarkDev <darkdev_brand@riseup.net>
2023-03-27 17:27:12 +0300

    alfa v. game progress screen

---
+++ src/lib/widgets/ui/GameProgressScreen.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { CountdownTimer } from '../../entities';
+</script>
+
+<div class="flex h-full flex-col justify-center">
+	<h2 class="text-center text-2xl text-white">Hejka</h2>
+	<div class="flex flex-row flex-wrap items-center ">
+		<!-- {#each levelsList as levelInfo}
+			<LevelCard on:click={() => selectMode(levelInfo)} {levelInfo} />
+		{/each} -->
+		<CountdownTimer />
+	</div>
+</div>
