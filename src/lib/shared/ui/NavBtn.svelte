c5a2832e081789f6357ad6ed00f90ad38b487bb4
DarkDev <darkdev_brand@riseup.net>
2023-03-27 17:27:48 +0300

    add footer & routing

---
+++ src/lib/shared/ui/NavBtn.svelte
@@ -0,0 +1,16 @@
+<script>
+	export let text, activeScreen;
+</script>
+
+<button on:click class:active={text === activeScreen}>
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		class="h-5 w-5"
+		fill="none"
+		viewBox="0 0 24 24"
+		stroke="currentColor"
+	>
+		<slot />
+	</svg>
+	<span class="btm-nav-label">{text}</span>
+</button>
