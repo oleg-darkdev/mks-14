4c408b9cebe736482c992a0d6d652d72ef8425bb
DarkDev <darkdev_brand@riseup.net>
2023-03-28 00:46:45 +0300

    add steps btn

---
+++ src/lib/entities/ui/StepsBtns.svelte
@@ -0,0 +1,24 @@
+<script>
+	const stepIncrease = () => step++;
+	const stepDecrease = () => step--;
+
+	export let step;
+</script>
+
+<div class="flex flex-row flex-wrap w-full justify-center">
+	{#if step != 1}
+		<button
+			on:click={() => stepDecrease()}
+			class="m-2 inline-block rounded-lg bg-gray-800 py-2 px-4 font-medium text-white shadow-md duration-150 hover:bg-gray-700 hover:shadow-none active:bg-gray-900"
+		>
+			Back
+		</button>
+	{/if}
+
+	<button
+		on:click={() => stepIncrease()}
+		class="m-2 inline-block rounded-lg bg-gray-800 py-2 px-4 font-medium text-white shadow-md duration-150 hover:bg-gray-700 hover:shadow-none active:bg-gray-900"
+	>
+		Next
+	</button>
+</div>
