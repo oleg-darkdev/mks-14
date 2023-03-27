c6138e4a63147581e03b56d903bd41637b4c6ba4
DarkDev <darkdev_brand@riseup.net>
2023-03-28 01:42:14 +0300

    rename instruction to manual

--- src/lib/widgets/index.js
+++ src/lib/widgets/index.js
@@ -1,6 +1,6 @@
 import FooterApp from './ui/FooterApp.svelte';
 import StartScreen from './ui/StartScreen.svelte';
 import GameProgressScreen from './ui/GameProgressScreen.svelte';
-import InstrustionScreen from './ui/InstrustionScreen.svelte';
+import ManualScreen from './ui/ManualScreen.svelte';
 
-export { FooterApp, StartScreen, GameProgressScreen, InstrustionScreen };
+export { FooterApp, StartScreen, GameProgressScreen, ManualScreen };
--- src/lib/widgets/ui/ManualScreen.svelte
+++ src/lib/widgets/ui/ManualScreen.svelte
@@ -1,13 +1,15 @@
 <script>
-  import { StepsList } from "../../entities";
+	import { StepsList, TableOfСontents } from '../../entities';
 
-    	const instrustionSteps = [
+	const instrustionSteps = [
 		{
 			title: 'Lorem ipsum is placeholder text',
 			desc: 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.',
 			img: ''
 		}
 	];
+
 </script>
 
-<StepsList steps={instrustionSteps}/>
+<!-- <StepsList steps={instrustionSteps}/> -->
+<TableOfСontents />
