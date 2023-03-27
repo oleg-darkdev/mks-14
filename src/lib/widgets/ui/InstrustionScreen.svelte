1b5d7c8c258cab534899e2cb49a0d22a04106495
DarkDev <darkdev_brand@riseup.net>
2023-03-27 17:27:04 +0300

    alfa v. instruction screen

---
+++ src/lib/widgets/ui/InstrustionScreen.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { StepsList } from "../../entities";
+
+    	const instrustionSteps = [
+		{
+			title: 'Lorem ipsum is placeholder text',
+			desc: 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.',
+			img: ''
+		}
+	];
+</script>
+
+<StepsList steps={instrustionSteps}/>
