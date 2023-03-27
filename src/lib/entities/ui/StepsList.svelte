caa76371f62bedac97050596e2ba899aa4ff4d2d
DarkDev <darkdev_brand@riseup.net>
2023-03-28 00:46:58 +0300

    alfa v. list steps

--- src/lib/entities/ui/StepsList.svelte
+++ src/lib/entities/ui/StepsList.svelte
@@ -1,17 +1,20 @@
 <script>
 	import { StepElement } from '../../shared';
+	$: stepsLength = steps.length - 1;
 
 	export let steps;
-
-	$: stepsLength = steps.length - 1;
 </script>
 
 <div
-	class="mx-auto px-4 py-16 sm:max-w-xl md:max-w-full md:px-24 lg:max-w-screen-xl lg:px-8 lg:py-20"
+	class="mx-auto h-full px-4 py-16 sm:max-w-xl md:max-w-full md:px-24 lg:max-w-screen-xl lg:px-8 lg:py-20"
 >
 	<div class="mx-auto grid max-w-2xl">
+		<slot />
+
 		{#each steps as step, idStep}
 			<StepElement {step} {idStep} {stepsLength} />
 		{/each}
+
+		<slot />
 	</div>
 </div>
