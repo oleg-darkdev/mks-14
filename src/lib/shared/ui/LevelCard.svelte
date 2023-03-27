69e901769ee43abf409b39bc411ab2e2e8d569a1
DarkDev <darkdev_brand@riseup.net>
2023-03-27 17:28:26 +0300

    alva v. level card

---
+++ src/lib/shared/ui/LevelCard.svelte
@@ -0,0 +1,21 @@
+<script>
+  export let levelInfo;
+</script>
+
+<div class="card card-height w-96 m-2 bg-primary text-primary-content">
+	<div class="card-body">
+		<h2 class="card-title">{levelInfo.title}</h2>
+		<p>{levelInfo.desc}</p>
+		<div class="card-actions justify-end">
+			<button on:click class="btn">Start</button>
+		</div>
+	</div>
+</div>
+
+
+<style>
+  .card-height {
+    max-height: 250px;
+    height: auto;
+  }
+</style>
