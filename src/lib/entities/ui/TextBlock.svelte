cb4f78558e2fe72453ac37045836dc7682eb69e7
DarkDev <darkdev_brand@riseup.net>
2023-03-28 01:41:24 +0300

    fix padding

--- src/lib/entities/ui/TextBlock.svelte
+++ src/lib/entities/ui/TextBlock.svelte
@@ -1,8 +1,8 @@
 <script>
-  export let title, desc;
+	export let title, desc;
 </script>
 
-<div class="py-14">
+<div class="py-6">
 	<div class="mx-auto max-w-screen-xl px-4 md:px-8 md:text-center">
 		<div class="max-w-xl md:mx-auto">
 			<h3 class="text-3xl font-semibold text-gray-300 sm:text-4xl">{title}</h3>
@@ -11,7 +11,7 @@
 			</p>
 		</div>
 		<div class="mt-4 flex items-center gap-3 md:justify-center">
-      <slot></slot>
+			<slot />
 		</div>
 	</div>
 </div>
