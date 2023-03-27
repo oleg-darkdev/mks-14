8424c42e125db04bcd9d8adaffe121a9e331b278
DarkDev <darkdev_brand@riseup.net>
2023-03-28 01:42:23 +0300

    alfa v.

--- src/lib/widgets/ui/GameProgressScreen.svelte
+++ src/lib/widgets/ui/GameProgressScreen.svelte
@@ -5,16 +5,98 @@
 </script>
 
 <div class="flex h-full flex-col justify-center">
-	<TextBlock
+	<!-- <TextBlock
 		title="Lorem ipsum is placeholder"
 		desc="Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups."
 	>
 		<StepsBtns bind:step />
 	</TextBlock>
-	<div class="flex flex-row flex-wrap items-center ">
-		<!-- {#each levelsList as levelInfo}
-			<LevelCard on:click={() => selectMode(levelInfo)} {levelInfo} />
-		{/each} -->
-		<CountdownTimer />
-	</div>
+
+	<CountdownTimer /> -->
+
+	{#if step == 1}
+		<TextBlock
+			title="Lorem ipsum is placeholder"
+			desc="Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups."
+		>
+			<StepsBtns bind:step />
+		</TextBlock>
+	{:else if step == 2}
+		<TextBlock
+			title="Lorem ipsum is placeholder"
+			desc="Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups."
+		>
+			<StepsBtns bind:step />
+		</TextBlock>
+	{:else if step == 3}
+		<TextBlock
+			title="Lorem ipsum is placeholder"
+			desc="Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups."
+		>
+			<StepsBtns bind:step />
+		</TextBlock>
+	{:else if step == 4}
+		<TextBlock
+			title="Lorem ipsum is placeholder"
+			desc="Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups."
+		>
+			<StepsBtns bind:step />
+		</TextBlock>
+	{:else if step == 5}
+		<TextBlock
+			title="Lorem ipsum is placeholder"
+			desc="Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups."
+		>
+			<StepsBtns bind:step />
+		</TextBlock>
+	{:else if step == 6}
+		<TextBlock
+			title="Lorem ipsum is placeholder"
+			desc="Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups."
+		>
+			<StepsBtns bind:step />
+		</TextBlock>
+	{:else if step == 7}
+		<TextBlock
+			title="Lorem ipsum is placeholder"
+			desc="Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups."
+		>
+			<StepsBtns bind:step />
+		</TextBlock>
+	{:else if step == 8}
+		<TextBlock
+			title="Lorem ipsum is placeholder"
+			desc="Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups."
+		>
+			<StepsBtns bind:step />
+		</TextBlock>
+	{:else if step == 9}
+		<TextBlock
+			title="Lorem ipsum is placeholder"
+			desc="Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups."
+		>
+			<StepsBtns bind:step />
+		</TextBlock>
+	{:else if step == 10}
+		<TextBlock
+			title="Lorem ipsum is placeholder"
+			desc="Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups."
+		>
+			<StepsBtns bind:step />
+		</TextBlock>
+	{:else if step == 11}
+		<TextBlock
+			title="Lorem ipsum is placeholder"
+			desc="Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups."
+		>
+			<StepsBtns bind:step />
+		</TextBlock>
+	{:else if step == 12}
+		<TextBlock
+			title="Lorem ipsum is placeholder"
+			desc="Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups."
+		>
+			<StepsBtns bind:step />
+		</TextBlock>
+	{/if}
 </div>
