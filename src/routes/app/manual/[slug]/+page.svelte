8f1ef42d5edd12f23680c3789633055779faf05e
DarkDev <darkdev_brand@riseup.net>
2023-03-30 00:10:45 +0300

    fix classes

--- src/routes/app/manual/[slug]/+page.svelte
+++ src/routes/app/manual/[slug]/+page.svelte
@@ -16,9 +16,10 @@
 			: $page.params.slug == 'experiments'
 			? experimentsTocData
 			: mainTocData;
+
 </script>
 
-<div class="flex h-full flex-col items-center justify-center pt-40 pb-10 p-2">
+<div class="flex h-full w-full flex-col items-center justify-center pt-20 pb-10">
 	<TableOfСontents {toc} />
 	<a href='/app' class="btn-block btn btn-lg btn-info mt-4  max-w-2xl">Home</a>
 </div>
