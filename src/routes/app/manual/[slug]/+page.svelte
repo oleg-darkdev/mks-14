bee0c9d62f8f6499c22a5840deee15236f2b803a
DarkDev <darkdev_brand@riseup.net>
2023-03-29 23:13:49 +0300

    alfa v.

---
+++ src/routes/app/manual/[slug]/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+	import { page } from '$app/stores';
+	import {
+		mainTocData,
+		personsTocData,
+		locationsTocData,
+		experimentsTocData
+	} from '../../../../lib/shared';
+	import { TableOfСontents } from '../../../../lib/entities';
+
+	$: toc =
+		$page.params.slug == 'locations'
+			? locationsTocData
+			: $page.params.slug == 'persons'
+			? personsTocData
+			: $page.params.slug == 'experiments'
+			? experimentsTocData
+			: mainTocData;
+</script>
+
+<div class="flex h-full flex-col items-center justify-center pt-40 pb-10 p-2">
+	<TableOfСontents {toc} />
+	<a href='/app' class="btn-block btn btn-lg btn-info mt-4  max-w-2xl">Home</a>
+</div>
