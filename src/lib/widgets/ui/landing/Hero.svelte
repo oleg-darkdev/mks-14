56f07058a60e28ba93a7bc1da6da11427ca2af2f
DarkDev <darkdev_brand@riseup.net>
2023-03-31 04:21:05 +0300

    add banner, upd colors & text

--- src/lib/widgets/ui/landing/Hero.svelte
+++ src/lib/widgets/ui/landing/Hero.svelte
@@ -1,23 +1,51 @@
-<section class="dark:bg-gray-800 dark:text-gray-100">
+<!-- <section class="dark:bg-gray-900 dark:text-gray-100">
 	<div
 		class="container mx-auto flex flex-col items-center px-4 py-16 text-center md:py-32 md:px-10 lg:px-32 xl:max-w-3xl"
 	>
 		<h1 class="text-4xl font-bold leading-none sm:text-5xl">
-			Quisquam necessita vel
-			<span class="dark:text-violet-400">laborum doloribus</span>delectus
+			Настольная популяризационная игра о космосе
+			<span class="dark:text-violet-400">и работе на МКС</span>
 		</h1>
 		<p class="mt-8 mb-12 px-8 text-lg">
 			Cupiditate minima voluptate temporibus quia? Architecto beatae esse ab amet vero eaque
 			explicabo!
 		</p>
 		<div class="flex flex-wrap justify-center">
-			<button
-				class="m-2 rounded px-8 py-3 text-lg font-semibold dark:bg-violet-400 dark:text-gray-900"
-				>Get started</button
+			<a href='./app'
+				class="m-2 rounded px-8 py-3 text-lg font-semibold dark:bg-violet-700 dark:text-gray-900"
+				>Get started</a
 			>
-			<button class="m-2 rounded border px-8 py-3 text-lg dark:border-gray-700 dark:text-gray-50"
-				>Learn more</button
+			<a href='#about' class="m-2 rounded border px-8 py-3 text-lg dark:border-gray-700 dark:text-gray-50"
+				>Learn more</a
 			>
+		</div>
+	</div>
+</section> -->
+
+<section class="hero min-h-screen" style="background-image: url(./images/banner.png);">
+	<div class="hero-overlay bg-opacity-60" />
+	<div class="hero-content text-center text-neutral-content">
+		<div class="mt-20 max-w-md">
+			<h1 class="text-4xl font-bold leading-none sm:text-5xl">
+				Настольная популяризационная игра о космосе
+				<span class="dark:text-violet-700">и работе на МКС</span>
+			</h1>
+			<!-- <p class="mt-8 mb-12 px-8 text-lg">
+				Cupiditate minima voluptate temporibus quia? Architecto beatae esse ab amet vero eaque
+				explicabo!
+			</p> -->
+			<div class="flex flex-wrap justify-center">
+				<a
+					href="./app"
+					class="m-2 rounded px-8 py-3 text-lg font-semibold hover:bg-white dark:bg-violet-700 dark:text-gray-900"
+					>Get started</a
+				>
+				<a
+					href="#about"
+					class="m-2 rounded border px-8 py-3 text-lg hover:bg-white hover:text-gray-900 dark:border-gray-700 dark:text-gray-50"
+					>Learn more</a
+				>
+			</div>
 		</div>
 	</div>
 </section>
