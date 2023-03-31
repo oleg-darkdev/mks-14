9d753398643491fe87f8abb7fe034be84ebaffbf
DarkDev <darkdev_brand@riseup.net>
2023-03-31 04:22:59 +0300

    refactoring alva v.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,16 @@
 <script>
-	import { HeaderLanding, HeroLanding, StatsLanding, FeaturesLanding, GalleryLanding, AppLanding, StepsLanding, ReviewsLanding, TeamLanding, FooterLanding } from '../lib/widgets/';
+	import {
+		HeaderLanding,
+		HeroLanding,
+		StatsLanding,
+		FeaturesLanding,
+		GalleryLanding,
+		AppLanding,
+		StepsLanding,
+		ReviewsLanding,
+		TeamLanding,
+		FooterLanding
+	} from '../lib/widgets/';
 </script>
 
 <HeaderLanding />
@@ -25,7 +36,7 @@
 <StepsLanding />
 
 <!-- pricing -->
-<!-- <section class="py-6 dark:bg-gray-800 dark:text-gray-50">
+<!-- <section class="py-6 dark:bg-gray-900 dark:text-gray-50">
 	<div class="container mx-auto flex flex-col items-center justify-center max-w-lg p-4 lg:max-w-full sm:p-10 lg:flex-row">
 		<div class="flex flex-col items-center justify-center flex-1 p-4 pb-8 sm:p-8 lg:p-16 dark:bg-gray-900">
 			<span class="text-sm">Basic</span>
@@ -37,7 +48,7 @@
 			<span class="text-sm font-semibold">Advanced</span>
 			<p class="text-5xl font-bold">89€</p>
 			<p class="font-semibold">Nemo deserunt possimus quo provident recusandae! Dolores qui architecto omnis pariatur eos voluptatibus sequi cum, non nesciunt aspernatur a?</p>
-			<button class="px-8 py-3 mt-6 text-lg font-semibold rounded sm:mt-12 dark:bg-gray-800 dark:text-gray-50">Sign up</button>
+			<button class="px-8 py-3 mt-6 text-lg font-semibold rounded sm:mt-12 dark:bg-gray-900 dark:text-gray-50">Sign up</button>
 		</div>
 	</div>
 </section> -->
