734d6febf54b874b42162b6441a9cf8a9c870f60
DarkDev <darkdev_brand@riseup.net>
2023-04-01 02:25:59 +0300

    fix padding for mobile

--- src/lib/widgets/ui/ManualScreen.svelte
+++ src/lib/widgets/ui/ManualScreen.svelte
@@ -16,8 +16,8 @@ import { page } from '$app/stores';
 
 <!-- <StepsList steps={instrustionSteps}/> -->
 
-<section class="flex h-screen flex-col items-center justify-center pb-10">
-	<div class="container mx-auto max-w-6xl space-y-6 p-6 sm:space-y-12">
+<section class="flex lg:min-h-screen xl:min-h-screen md:min-h-screen h-auto flex-col items-center justify-center xl:pb-10 md:pb-14 pb-36 lg:pb-10">
+	<div class="container mx-auto max-w-6xl space-y-6 xl:p-6 lg:p-6 md:p-4 p-2 ">
     <TableOfСontents {toc} />
 	</div>
 </section>
