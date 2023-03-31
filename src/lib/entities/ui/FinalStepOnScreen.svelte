1e1512da137005ac9972b0491f0418a907763853
DarkDev <darkdev_brand@riseup.net>
2023-03-31 22:21:34 +0300

    add colors

--- src/lib/entities/ui/FinalStepOnScreen.svelte
+++ src/lib/entities/ui/FinalStepOnScreen.svelte
@@ -2,7 +2,7 @@
 	export let desc, title, btnText;
 </script>
 
-<div class="py-14">
+<div class="py-14 border-2 border-violet-700 rounded-lg shadow-md shadow-violet-700 ">
 	<div class="mx-auto max-w-screen-xl px-4 md:px-8 md:text-center">
 		<div class="max-w-xl md:mx-auto">
 			<h3 class="text-3xl font-semibold text-gray-300 sm:text-4xl">{title}</h3>
@@ -15,7 +15,7 @@
 		<div class="mt-4 flex items-center gap-3 md:justify-center">
 			<button
 				on:click
-				class="m-2 inline-block rounded-lg bg-gray-900 py-2 px-4 font-medium text-white shadow-md duration-150 hover:bg-gray-700 hover:shadow-none active:bg-gray-900"
+				class="m-2 inline-block rounded-lg bg-violet-700 py-2 px-4 font-medium text-white shadow-md duration-150 hover:bg-gray-700 hover:shadow-none active:bg-gray-900"
 			>
 				{btnText}
 			</button>
