d422f212a06f56f15ec4ca51c79b506b2d01bd57
DarkDev <darkdev_brand@riseup.net>
2023-03-31 04:22:21 +0300

    upd bg color, fix text

--- src/lib/widgets/ui/landing/App.svelte
+++ src/lib/widgets/ui/landing/App.svelte
@@ -1,19 +1,18 @@
-<section class="p-6 py-12 dark:bg-violet-400 dark:text-gray-900">
+<section class="p-6 py-12 bg-violet-700 dark:text-gray-900">
 	<div class="container mx-auto">
 		<div class="flex flex-col items-center justify-between lg:flex-row">
 			<h2 class="text-center text-6xl font-bold tracking-tighter">
-				Up to
-				<br class="sm:hidden" />50% Off
+				Бесплатное приложение
 			</h2>
 			<div class="space-x-2 py-2 text-center lg:py-0">
-				<span>Plus free shipping! Use code:</span>
-				<span class="text-lg font-bold">MAMBA</span>
+				<span>Для расширения горизонтов настольной игры</span>
+				<span class="text-lg font-bold">MKS-14</span>
 			</div>
 			<a
 				href="#"
 				rel="noreferrer noopener"
 				class="mt-4 block rounded-md border px-5 py-3 dark:border-gray-400 dark:bg-gray-50 dark:text-gray-900 lg:mt-0"
-				>Shop Now</a
+				>Go to App</a
 			>
 		</div>
 	</div>
