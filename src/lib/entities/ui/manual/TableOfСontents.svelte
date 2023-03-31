3bfb2933ffd653baef977aae53bc7fc3b9e5c026
DarkDev <darkdev_brand@riseup.net>
2023-03-31 20:00:15 +0300

    add id for card

--- src/lib/entities/ui/manual/TableOfСontents.svelte
+++ src/lib/entities/ui/manual/TableOfСontents.svelte
@@ -2,26 +2,28 @@
 	export let toc;
 </script>
 
-<div class="grid grid-cols-1 justify-center gap-6 sm:grid-cols-2 lg:grid-cols-3">
-	{#each toc.data as navigation}
+<div class="grid grid-cols-1 justify-center gap-6 sm:grid-cols-2 lg:grid-cols-3 ">
+	{#each toc.data as navigation, index}
 		<a
 			rel="noopener noreferrer"
 			href="{toc.link}{navigation.id}"
-			class="group mx-auto max-w-sm border-2 border-violet-700 shadow-md shadow-violet-700 hover:no-underline focus:no-underline dark:bg-gray-900"
+			class="group mx-auto max-w-sm rounded-lg border-2 border-violet-700 shadow-md shadow-violet-700 hover:bg-violet-700 hover:no-underline focus:no-underline dark:bg-gray-900"
 		>
 			<img
 				role="presentation"
-				class="h-44 w-full rounded object-cover dark:bg-gray-500"
+				class="h-44 w-full rounded-lg object-cover dark:bg-gray-500"
 				src={navigation.img}
 				alt={navigation.id}
 			/>
-			<div class="space-y-2 p-6">
-				<h3 class="text-2xl font-semibold group-hover:underline group-focus:underline">
-					{navigation.title}
+			<div class="space-y-2 p-6 group-focus:bg-violet-700 ">
+				<h3 class="text-2xl font-semibold group-hover:underline group-focus:underline ">
+					<span class="text-violet-700 group-hover:text-gray-100 group-focus:text-gray-100 "
+						>#{index + 1}
+					</span>
+          {navigation.title}
 				</h3>
 				<p>{navigation.shortDesc}</p>
 			</div>
 		</a>
 	{/each}
 </div>
-
