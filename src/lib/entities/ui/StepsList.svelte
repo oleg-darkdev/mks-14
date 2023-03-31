beb6703938aca862531608a8d0f475d742c6d891
DarkDev <darkdev_brand@riseup.net>
2023-03-31 22:24:06 +0300

    add colors, del top slot

--- src/lib/entities/ui/StepsList.svelte
+++ src/lib/entities/ui/StepsList.svelte
@@ -6,11 +6,9 @@
 </script>
 
 <div
-	class="mx-auto px-4 py-4 sm:max-w-xl md:max-w-full md:px-24 lg:max-w-screen-xl lg:px-8 lg:py-4"
+	class="mx-auto border border-violet-700 shadow-md shadow-violet-700 rounded-lg px-4 py-4 sm:max-w-xl md:max-w-full md:px-24 lg:max-w-screen-xl lg:px-8 lg:py-4"
 >
 	<div class="mx-auto grid max-w-2xl">
-		<!-- <slot /> -->
-
 		{#each steps as step, idStep}
 			<StepElement {step} {idStep} {stepsLength} />
 		{/each}
