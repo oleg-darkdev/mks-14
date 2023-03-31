082fefb2ff32ad4c1c0952b0ed014cd8b67ae287
DarkDev <darkdev_brand@riseup.net>
2023-03-31 04:22:44 +0300

    refactoring

---
+++ src/lib/shared/ui/landing/VerticalImage.svelte
@@ -0,0 +1,12 @@
+<script>
+
+  export let img, alt;
+</script>
+
+
+	<img
+		src="{img}"
+		alt="{alt}"
+		class="mx-auto rounded-lg border-2 border-violet-700 shadow-lg shadow-violet-700  dark:bg-gray-500"
+	/>
+
