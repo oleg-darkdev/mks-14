30e41e5d6daf6c6bd5ca3ce4d67162030a60045a
DarkDev <darkdev_brand@riseup.net>
2023-03-31 04:20:37 +0300

    refactoring alva v.

--- src/lib/widgets/ui/landing/Gallery.svelte
+++ src/lib/widgets/ui/landing/Gallery.svelte
@@ -1,54 +1,27 @@
-<section class="py-6 dark:bg-gray-800 dark:text-gray-50">
+<script>
+  import { SmallSquareImage } from "../../../shared";
+</script>
+
+<section class="py-6 dark:bg-gray-900 dark:text-gray-50">
 	<div class="container mx-auto grid grid-cols-2 gap-4 p-4 md:grid-cols-4">
 		<img
-			src="https://source.unsplash.com/random/301x301/"
-			alt=""
-			class="min-h-96 col-span-2 row-span-2 aspect-square h-full w-full rounded shadow-sm dark:bg-gray-500 md:col-start-3 md:row-start-1"
+			src="./images/gallery_big_1.png"
+			alt="Big gallery image 1"
+			class="min-h-96 col-span-2 row-span-2 aspect-square h-full w-full rounded shadow-md border-2 border-violet-700 shadow-violet-700  dark:bg-violet-700 md:col-start-3 md:row-start-1"
 		/>
+    <SmallSquareImage img='./images/gallery_1.png' alt='gallery image 1'/>
+    <SmallSquareImage img='./images/gallery_2.png' alt='gallery image 2'/>
+    <SmallSquareImage img='./images/gallery_3.png' alt='gallery image 3'/>
+    <SmallSquareImage img='./images/gallery_4.png' alt='gallery image 4'/>
+    <SmallSquareImage img='./images/gallery_5.png' alt='gallery image 5'/>
+    <SmallSquareImage img='./images/gallery_6.png' alt='gallery image 6'/>
+    <SmallSquareImage img='./images/gallery_7.png' alt='gallery image 7'/>
+    <SmallSquareImage img='./images/gallery_8.png' alt='gallery image 8'/>
 		<img
-			alt=""
-			class="min-h-48 aspect-square h-full w-full rounded shadow-sm dark:bg-gray-500"
-			src="https://source.unsplash.com/random/200x200/?0"
+			src="./images/gallery_big_2.png"
+			alt="Big gallery image 2"
+    	class="min-h-96 col-span-2 row-span-2 aspect-square h-full w-full rounded shadow-md border-2 border-violet-700 shadow-violet-700 dark:bg-violet-700 md:col-start-1 md:row-start-3"
 		/>
-		<img
-			alt=""
-			class="min-h-48 aspect-square h-full w-full rounded shadow-sm dark:bg-gray-500"
-			src="https://source.unsplash.com/random/200x200/?1"
-		/>
-		<img
-			alt=""
-			class="min-h-48 aspect-square h-full w-full rounded shadow-sm dark:bg-gray-500"
-			src="https://source.unsplash.com/random/200x200/?2"
-		/>
-		<img
-			alt=""
-			class="min-h-48 aspect-square h-full w-full rounded shadow-sm dark:bg-gray-500"
-			src="https://source.unsplash.com/random/200x200/?3"
-		/>
-		<img
-			alt=""
-			class="min-h-48 aspect-square h-full w-full rounded shadow-sm dark:bg-gray-500"
-			src="https://source.unsplash.com/random/200x200/?4"
-		/>
-		<img
-			alt=""
-			class="min-h-48 aspect-square h-full w-full rounded shadow-sm dark:bg-gray-500"
-			src="https://source.unsplash.com/random/200x200/?5"
-		/>
-		<img
-			alt=""
-			class="min-h-48 aspect-square h-full w-full rounded shadow-sm dark:bg-gray-500"
-			src="https://source.unsplash.com/random/200x200/?6"
-		/>
-		<img
-			alt=""
-			class="min-h-48 aspect-square h-full w-full rounded shadow-sm dark:bg-gray-500"
-			src="https://source.unsplash.com/random/200x200/?7"
-		/>
-		<img
-			src="https://source.unsplash.com/random/302x302/"
-			alt=""
-			class="min-h-96 col-span-2 row-span-2 aspect-square h-full w-full rounded shadow-sm dark:bg-gray-500 md:col-start-1 md:row-start-3"
-		/>
+
 	</div>
 </section>
