0f5508d30b61098c7633057197597505967ece27
DarkDev <darkdev_brand@riseup.net>
2023-03-31 22:21:10 +0300

    upd text & color

--- src/lib/entities/ui/welcomeScreen/RolesSetup.svelte
+++ src/lib/entities/ui/welcomeScreen/RolesSetup.svelte
@@ -7,7 +7,7 @@
   export let step;
 </script>
 
-<div class="py-14">
+<div class="py-14 border-2 border-violet-700 rounded-lg shadow-md shadow-violet-700 ">
 	<div class="mx-auto max-w-screen-xl px-4 md:px-8 md:text-center">
 		<div class="max-w-xl md:mx-auto mb-4">
 			<h3 class="text-3xl font-semibold text-gray-300 sm:text-4xl">Roles setup</h3>
@@ -24,12 +24,12 @@
             peoplesOnGame = variant;
             step++;
           }}
-					class="rounded-box border-2 border-violet-700 bg-violet-700 rounded-lg shadow-md shadow-violet-700 flex flex-col items-center  p-2 text-neutral-content "
+					class="rounded-box hover:bg-gray-100 hover:text-violet-900  border-2 border-violet-700 bg-violet-700 rounded-lg shadow-md shadow-violet-700 flex flex-col items-center  p-2 text-neutral-content "
 				>
 					<span class="font-mono countdown text-center text-5xl">
 						{variant}
 					</span>
-					<span> peolpes </span>
+					<span> players </span>
 				</button>
 			{/each}
 		</div>
