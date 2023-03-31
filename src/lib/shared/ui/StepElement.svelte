0f2473342b5e219e88128dea862ffaa0855b802d
DarkDev <darkdev_brand@riseup.net>
2023-03-31 21:10:39 +0300

    upd colors

--- src/lib/shared/ui/StepElement.svelte
+++ src/lib/shared/ui/StepElement.svelte
@@ -4,22 +4,22 @@
 
 <div class="flex ">
 	<div class="mr-6 flex flex-col items-center ">
-		<div class="h-10 w-px {idStep == 0 ? 'opacity-0' : 'bg-gray-300'} sm:h-full" />
+		<div class="h-10 w-px {idStep == 0 ? 'opacity-0' : 'bg-violet-700 '} sm:h-full" />
 		<div>
-			<div class="flex h-8 w-8 items-center justify-center rounded-full border text-xs font-medium">
+			<div class="flex text-violet-700 h-8 w-8 items-center justify-center rounded-full border border-violet-700 text-xs font-medium">
 				{idStep + 1}
 			</div>
 		</div>
-		<div class="h-full w-px {stepsLength == idStep ? 'opacity-0' : 'bg-gray-300'} " />
+		<div class="h-full w-px {stepsLength == idStep ? 'opacity-0' : 'bg-violet-700 '} " />
 	</div>
 	<div class="flex flex-col pb-6 sm:flex-row sm:items-center sm:pb-0">
 		<div class="sm:mr-5">
 			<div
-				class="my-3 flex h-16 w-16 items-center justify-center rounded-full bg-indigo-50 sm:h-24 sm:w-24"
+				class="my-3 border border-violet-700 shadow-md shadow-violet-700 flex h-16 w-16 items-center justify-center rounded-full bg-indigo-50 sm:h-24 sm:w-24"
 			>
 				<svg
 					class="text-deep-purple-accent-400 h-12 w-12 sm:h-16 sm:w-16"
-					stroke="currentColor"
+					stroke="#661AE6"
 					viewBox="0 0 52 52"
 				>
 					<polygon
@@ -33,7 +33,7 @@
 			</div>
 		</div>
 		<div>
-			<p class="text-xl font-semibold sm:text-base">{step.title}</p>
+			<p class="text-xl font-semibold text-violet-700">{step.title}</p>
 			<p class="text-sm text-gray-400">
 				{step.desc}
 			</p>
