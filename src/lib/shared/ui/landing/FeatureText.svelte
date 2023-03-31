90f57e9f22dc9963c307e639adf2419a090e6706
DarkDev <darkdev_brand@riseup.net>
2023-03-31 04:21:33 +0300

    refactoring alva v.

---
+++ src/lib/shared/ui/landing/FeatureText.svelte
@@ -0,0 +1,27 @@
+<script>
+  export let text;
+</script>
+
+<div class="flex">
+	<div class="flex-shrink-0">
+		<div
+			class="flex h-12 w-12 items-center justify-center rounded-md dark:bg-violet-700 dark:text-gray-900"
+		>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke="currentColor"
+				class="h-7 w-7"
+			>
+				<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+			</svg>
+		</div>
+	</div>
+	<div class="ml-4">
+		<h4 class="text-lg font-medium leading-6 dark:text-gray-50">{text.title}</h4>
+		<p class="mt-2 dark:text-gray-400">
+			{text.desc}
+		</p>
+	</div>
+</div>
--- src/lib/widgets/ui/landing/Features.svelte
+++ src/lib/widgets/ui/landing/Features.svelte
@@ -1,8 +1,62 @@
-<section class="dark:bg-gray-800 dark:text-gray-100">
+<script>
+	import { FeatureText, VerticalImage } from '../../../shared/';
+
+	let featuresBoardgame = [
+		{
+			title: 'Hard scify',
+			desc: 'Per odio fabellas consulatu cu. Utroque detracto mel ea, quo te latine theophrastus. Ea his tale nibh dissentias, mei exerci tamquam euripidis cu.'
+		},
+		{
+			title: 'Реальные эксперименты',
+			desc: 'Per odio fabellas consulatu cu. Utroque detracto mel ea, quo te latine theophrastus. Ea his tale nibh dissentias, mei exerci tamquam euripidis cu.'
+		},
+		{
+			title: 'Реальные люди',
+			desc: 'Per odio fabellas consulatu cu. Utroque detracto mel ea, quo te latine theophrastus. Ea his tale nibh dissentias, mei exerci tamquam euripidis cu.'
+		},
+		{
+			title: 'Кооперативная игра',
+			desc: 'Per odio fabellas consulatu cu. Utroque detracto mel ea, quo te latine theophrastus. Ea his tale nibh dissentias, mei exerci tamquam euripidis cu.'
+		},
+		{
+			title: 'Доступна для PnP',
+			desc: 'Per odio fabellas consulatu cu. Utroque detracto mel ea, quo te latine theophrastus. Ea his tale nibh dissentias, mei exerci tamquam euripidis cu.'
+		},
+		{
+			title: 'Qr коды',
+			desc: 'Для быстрого получения справочной информации о экспериментах и персонажах, метеоритах.'
+		}
+	];
+
+	let featuresApp = [
+		{
+			title: 'Электронный помощник для сетапа игры',
+			desc: 'Per odio fabellas consulatu cu. Utroque detracto mel ea, quo te latine theophrastus. Ea his tale nibh dissentias, mei exerci tamquam euripidis cu.'
+		},
+		{
+			title: 'Справочная информация',
+			desc: 'Для быстрого получения справочной информации о экспериментах и персонажах.'
+		},
+		{
+			title: 'Электронная инструкция',
+			desc: 'Per odio fabellas consulatu cu. Utroque detracto mel ea, quo te latine theophrastus. Ea his tale nibh dissentias, mei exerci tamquam euripidis cu.'
+		},
+		{
+			title: 'Ответы на часто задоваемые вопросы',
+			desc: 'Per odio fabellas consulatu cu. Utroque detracto mel ea, quo te latine theophrastus. Ea his tale nibh dissentias, mei exerci tamquam euripidis cu.'
+		},
+		{
+			title: 'Открытый исходный код на github',
+			desc: 'Per odio fabellas consulatu cu. Utroque detracto mel ea, quo te latine theophrastus. Ea his tale nibh dissentias, mei exerci tamquam euripidis cu.'
+		}
+	];
+</script>
+
+<section class="dark:bg-gray-900 dark:text-gray-100">
 	<div class="container mx-auto max-w-xl space-y-24 p-6 py-12 lg:max-w-7xl lg:px-8">
 		<div>
 			<h2 class="text-center text-3xl font-bold tracking-tight dark:text-gray-50 sm:text-5xl">
-				Aliquip definiebas ad est
+				Популяризационная настольная игра
 			</h2>
 			<p class="mx-auto mt-4 max-w-3xl text-center text-xl dark:text-gray-400">
 				Quando cetero his ne, eum admodum sapientem ut.
@@ -11,228 +65,41 @@
 		<div class="grid lg:grid-cols-2 lg:items-center lg:gap-8">
 			<div>
 				<h3 class="text-2xl font-bold tracking-tight dark:text-gray-50 sm:text-3xl">
-					Ad vix debet docendi
+					Настольная игра
 				</h3>
 				<p class="mt-3 text-lg dark:text-gray-400">
 					Ne dicta praesent ocurreret has, diam theophrastus at pro. Eos etiam regione ut, persius
 					eripuit quo id. Sit te euismod tacimates.
 				</p>
 				<div class="mt-12 space-y-12">
-					<div class="flex">
-						<div class="flex-shrink-0">
-							<div
-								class="flex h-12 w-12 items-center justify-center rounded-md dark:bg-violet-400 dark:text-gray-900"
-							>
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									fill="none"
-									viewBox="0 0 24 24"
-									stroke="currentColor"
-									class="h-7 w-7"
-								>
-									<path
-										stroke-linecap="round"
-										stroke-linejoin="round"
-										stroke-width="2"
-										d="M5 13l4 4L19 7"
-									/>
-								</svg>
-							</div>
-						</div>
-						<div class="ml-4">
-							<h4 class="text-lg font-medium leading-6 dark:text-gray-50">
-								Per ei quaeque sensibus
-							</h4>
-							<p class="mt-2 dark:text-gray-400">
-								Ex usu illum iudico molestie. Pro ne agam facete mediocritatem, ridens labore facete
-								mea ei. Pro id apeirian dignissim.
-							</p>
-						</div>
-					</div>
-					<div class="flex">
-						<div class="flex-shrink-0">
-							<div
-								class="flex h-12 w-12 items-center justify-center rounded-md dark:bg-violet-400 dark:text-gray-900"
-							>
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									fill="none"
-									viewBox="0 0 24 24"
-									stroke="currentColor"
-									class="h-7 w-7"
-								>
-									<path
-										stroke-linecap="round"
-										stroke-linejoin="round"
-										stroke-width="2"
-										d="M5 13l4 4L19 7"
-									/>
-								</svg>
-							</div>
-						</div>
-						<div class="ml-4">
-							<h4 class="text-lg font-medium leading-6 dark:text-gray-50">
-								Cu imperdiet posidonium sed
-							</h4>
-							<p class="mt-2 dark:text-gray-400">
-								Amet utinam aliquando ut mea, malis admodum ocurreret nec et, elit tibique cu nec.
-								Nec ex maluisset inciderint, ex quis.
-							</p>
-						</div>
-					</div>
-					<div class="flex">
-						<div class="flex-shrink-0">
-							<div
-								class="flex h-12 w-12 items-center justify-center rounded-md dark:bg-violet-400 dark:text-gray-900"
-							>
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									fill="none"
-									viewBox="0 0 24 24"
-									stroke="currentColor"
-									class="h-7 w-7"
-								>
-									<path
-										stroke-linecap="round"
-										stroke-linejoin="round"
-										stroke-width="2"
-										d="M5 13l4 4L19 7"
-									/>
-								</svg>
-							</div>
-						</div>
-						<div class="ml-4">
-							<h4 class="text-lg font-medium leading-6 dark:text-gray-50">
-								Nulla omittam sadipscing mel ne
-							</h4>
-							<p class="mt-2 dark:text-gray-400">
-								At sed possim oporteat probatus, justo graece ne nec, minim commodo legimus ut vix.
-								Ut eos iudico quando soleat, nam modus.
-							</p>
-						</div>
-					</div>
+					{#each featuresBoardgame as text}
+						<FeatureText {text} />
+					{/each}
 				</div>
 			</div>
 			<div aria-hidden="true" class="mt-10 lg:mt-0">
-				<img
-					src="https://source.unsplash.com/random/360x480"
-					alt=""
-					class="mx-auto rounded-lg shadow-lg dark:bg-gray-500"
-				/>
+				<VerticalImage img="./images/features_boardgame.png " alt="Boardgame" />
 			</div>
 		</div>
+
 		<div>
 			<div class="grid lg:grid-cols-2 lg:items-center lg:gap-8">
 				<div class="lg:col-start-2">
 					<h3 class="text-2xl font-bold tracking-tight dark:text-gray-50 sm:text-3xl">
-						Eam nibh gloriatur ex
+						Веб приложение
 					</h3>
 					<p class="mt-3 text-lg dark:text-gray-400">
 						Per odio fabellas consulatu cu. Utroque detracto mel ea, quo te latine theophrastus. Ea
 						his tale nibh dissentias, mei exerci tamquam euripidis cu.
 					</p>
 					<div class="mt-12 space-y-12">
-						<div class="flex">
-							<div class="flex-shrink-0">
-								<div
-									class="flex h-12 w-12 items-center justify-center rounded-md dark:bg-violet-400 dark:text-gray-900"
-								>
-									<svg
-										xmlns="http://www.w3.org/2000/svg"
-										fill="none"
-										viewBox="0 0 24 24"
-										stroke="currentColor"
-										class="h-7 w-7"
-									>
-										<path
-											stroke-linecap="round"
-											stroke-linejoin="round"
-											stroke-width="2"
-											d="M5 13l4 4L19 7"
-										/>
-									</svg>
-								</div>
-							</div>
-							<div class="ml-4">
-								<h4 class="text-lg font-medium leading-6 dark:text-gray-50">
-									Cibo augue offendit has ad
-								</h4>
-								<p class="mt-2 dark:text-gray-400">
-									An per velit appellantur, ut utinam minimum nominavi sit, odio nostro habemus ne
-									nec. Ne sonet regione contentiones est.
-								</p>
-							</div>
-						</div>
-						<div class="flex">
-							<div class="flex-shrink-0">
-								<div
-									class="flex h-12 w-12 items-center justify-center rounded-md dark:bg-violet-400 dark:text-gray-900"
-								>
-									<svg
-										xmlns="http://www.w3.org/2000/svg"
-										fill="none"
-										viewBox="0 0 24 24"
-										stroke="currentColor"
-										class="h-7 w-7"
-									>
-										<path
-											stroke-linecap="round"
-											stroke-linejoin="round"
-											stroke-width="2"
-											d="M5 13l4 4L19 7"
-										/>
-									</svg>
-								</div>
-							</div>
-							<div class="ml-4">
-								<h4 class="text-lg font-medium leading-6 dark:text-gray-50">
-									At eum ferri luptatum lobortis
-								</h4>
-								<p class="mt-2 dark:text-gray-400">
-									Te per quidam maiorum ocurreret, etiam delicatissimi usu ad. Ne has quod
-									periculis. Te sit primis iisque efficiantur.
-								</p>
-							</div>
-						</div>
-						<div class="flex">
-							<div class="flex-shrink-0">
-								<div
-									class="flex h-12 w-12 items-center justify-center rounded-md dark:bg-violet-400 dark:text-gray-900"
-								>
-									<svg
-										xmlns="http://www.w3.org/2000/svg"
-										fill="none"
-										viewBox="0 0 24 24"
-										stroke="currentColor"
-										class="h-7 w-7"
-									>
-										<path
-											stroke-linecap="round"
-											stroke-linejoin="round"
-											stroke-width="2"
-											d="M5 13l4 4L19 7"
-										/>
-									</svg>
-								</div>
-							</div>
-							<div class="ml-4">
-								<h4 class="text-lg font-medium leading-6 dark:text-gray-50">
-									Dicunt verterem evertitur eu sea
-								</h4>
-								<p class="mt-2 dark:text-gray-400">
-									Audire principes rationibus eam an, autem nominavi luptatum per te. Sumo fabulas
-									vim eu, sonet saperet eleifend ut vix.
-								</p>
-							</div>
-						</div>
+						{#each featuresApp as text}
+							<FeatureText {text} />
+						{/each}
 					</div>
 				</div>
 				<div class="mt-10 lg:col-start-1 lg:row-start-1 lg:mt-0">
-					<img
-						src="https://source.unsplash.com/random/361x481"
-						alt=""
-						class="mx-auto rounded-lg shadow-lg dark:bg-gray-500"
-					/>
+					<VerticalImage img="./images/features_app.png " alt="App" />
 				</div>
 			</div>
 		</div>
