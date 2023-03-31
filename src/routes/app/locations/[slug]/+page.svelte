c6d576179f47c292c72cbe1508069d5362c6a81b
DarkDev <darkdev_brand@riseup.net>
2023-03-31 17:36:41 +0300

    add recommendation section, banner

--- src/routes/app/locations/[slug]/+page.svelte
+++ src/routes/app/locations/[slug]/+page.svelte
@@ -1,5 +1,34 @@
 <script>
-  import {Article} from "../../../../lib/shared/";
+	import { Article } from '../../../../lib/shared/';
+	import { page } from '$app/stores';
+
+	import {
+		mainTocData,
+		personsTocData,
+		locationsTocData,
+		experimentsTocData
+	} from '../../../../lib/shared';
+
+	const currentUrl = window.location.pathname;
+	function getLocationFromUrl(urlString) {
+		const urlArray = urlString.split('/'),
+			location = urlArray[2];
+
+		return location;
+	}
+
+	const locationValue = getLocationFromUrl(currentUrl);
+
+	const textArray =
+		locationValue == 'locations'
+			? locationsTocData
+			: locationValue == 'persons'
+			? personsTocData
+			: locationValue == 'experiments'
+			? experimentsTocData
+			: mainTocData;
+
+	const thisArticleText = textArray.data[$page.params.slug - 1];
 </script>
 
-<Article/>
+<Article manualArticle={thisArticleText} />
