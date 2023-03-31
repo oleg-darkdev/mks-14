b1418071fa1f60407484f9bea2b47e0b044afda1
DarkDev <darkdev_brand@riseup.net>
2023-03-31 04:22:02 +0300

    upd bg color, clear btn

--- src/lib/widgets/ui/landing/Header.svelte
+++ src/lib/widgets/ui/landing/Header.svelte
@@ -1,4 +1,4 @@
-<header class="p-4 dark:bg-gray-800 dark:text-gray-100">
+<header class="p-4 dark:bg-gray-900 dark:text-gray-100">
 	<div class="container mx-auto flex h-16 justify-between md:justify-center md:space-x-8">
 		<ul class="hidden items-stretch space-x-3 md:flex">
 			<li class="flex">
@@ -19,7 +19,7 @@
 				<a
 					rel="noopener noreferrer"
 					href="#gallery"
-					class="-mb-1 flex items-center border-b-2 px-4 dark:border-transparent dark:border-violet-400 dark:text-violet-400"
+					class="-mb-1 flex items-center border-b-2 px-4 dark:border-transparent "
 					>Gallery</a
 				>
 			</li>
@@ -55,21 +55,5 @@
 				>
 			</li>
 		</ul>
-		<button title="Button" type="button" class="p-4 md:hidden">
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				fill="none"
-				viewBox="0 0 24 24"
-				stroke="currentColor"
-				class="h-6 w-6 dark:text-gray-100"
-			>
-				<path
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					stroke-width="2"
-					d="M4 6h16M4 12h16M4 18h16"
-				/>
-			</svg>
-		</button>
 	</div>
 </header>
