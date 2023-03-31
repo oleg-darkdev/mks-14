a3f1551b63a14a0cfe6b54fd35940b0b691bc55b
DarkDev <darkdev_brand@riseup.net>
2023-03-31 19:44:44 +0300

    upd api

--- src/lib/shared/index.js
+++ src/lib/shared/index.js
@@ -9,30 +9,32 @@ import FeatureText from './ui/landing/FeatureText.svelte';
 import VerticalImage from './ui/landing/VerticalImage.svelte';
 import SmallSquareImage from './ui/landing/SmallSquareImage.svelte';
 
-
 import mainTocData from './data/toc/mainToc';
 import personsTocData from './data/toc/personsToc';
 import locationsTocData from './data/toc/locationsToc';
 import experimentsTocData from './data/toc/experimentsToc';
 
+// utils
+import getCurrentUrl from './utils/getCurrentUrl';
+import getLocationFromUrl from './utils/getLocationFromUrl';
+import identifySelectedData from './utils/identifySelectedData';
+
 
 export {
 	NavBtn,
 	LevelCard,
-  StepElement,
-  Article,
-  LargeBtn,
-  HeaderLink,
-
-
-  FeatureText,
-  VerticalImage,
-  SmallSquareImage,
-
-
+	StepElement,
+	Article,
+	LargeBtn,
+	HeaderLink,
+	FeatureText,
+	VerticalImage,
+	SmallSquareImage,
 	mainTocData,
 	personsTocData,
 	locationsTocData,
 	experimentsTocData,
-
+	getCurrentUrl,
+	getLocationFromUrl,
+	identifySelectedData
 };
--- src/lib/widgets/index.js
+++ src/lib/widgets/index.js
@@ -1,4 +1,4 @@
-import FooterApp from './ui/FooterApp.svelte';
+import FooterApp from './ui/app/FooterApp.svelte';
 import StartScreen from './ui/StartScreen.svelte';
 import GameProgressScreen from './ui/GameProgressScreen.svelte';
 import ManualScreen from './ui/ManualScreen.svelte';
