b79de34aa04f59bf0fc6103ed4b336ef8150b5fa
DarkDev <darkdev_brand@riseup.net>
2023-03-31 17:38:27 +0300

    refactoring link header

---
+++ src/lib/shared/ui/HeaderLink.svelte
@@ -0,0 +1,9 @@
+<script>
+  export let link;
+</script>
+
+<a
+	rel="noopener noreferrer"
+	href="{link.link}"
+	class="-mb-1 flex items-center border-b-2 px-4 dark:border-transparent">{link.text}</a
+>
