1671053d25f9732582ef1250323c909e1ac22b86
DarkDev <darkdev_brand@riseup.net>
2023-03-31 21:07:52 +0300

    upd classes, clear slot for btn`s

--- src/lib/entities/ui/StepsList.svelte
+++ src/lib/entities/ui/StepsList.svelte
@@ -6,10 +6,10 @@
 </script>
 
 <div
-	class="mx-auto h-full px-4 py-16 sm:max-w-xl md:max-w-full md:px-24 lg:max-w-screen-xl lg:px-8 lg:py-20"
+	class="mx-auto px-4 py-4 sm:max-w-xl md:max-w-full md:px-24 lg:max-w-screen-xl lg:px-8 lg:py-4"
 >
 	<div class="mx-auto grid max-w-2xl">
-		<slot />
+		<!-- <slot /> -->
 
 		{#each steps as step, idStep}
 			<StepElement {step} {idStep} {stepsLength} />
