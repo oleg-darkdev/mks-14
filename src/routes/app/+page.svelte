18566757a833bfe2bb0fcefc7f7dc29d76cd2377
DarkDev <darkdev_brand@riseup.net>
2023-04-04 03:57:23 +0300

    add styles

--- src/routes/app/+page.svelte
+++ src/routes/app/+page.svelte
@@ -4,14 +4,20 @@
 	let activeScreen = 'Rozpoczęcie';
 </script>
 
-<section class="flex min-h-screen flex-col items-center justify-center">
+<section class="flex min-h-screen flex-col items-center  justify-center pt-10 pb-40">
 	<div class:hidden={activeScreen == 'Instrukcja' || activeScreen == 'Rozgrywka'}>
 		<StartScreen bind:activeScreen />
 	</div>
-	<div class:hidden={activeScreen == 'Rozpoczęcie' || activeScreen == 'Rozgrywka'}>
+	<div
+		class="flex h-auto flex-col items-center justify-center pb-36 md:min-h-screen md:pb-14 lg:min-h-screen lg:pb-10 xl:min-h-screen xl:pb-10"
+		class:hidden={activeScreen == 'Rozpoczęcie' || activeScreen == 'Rozgrywka'}
+	>
 		<ManualScreen />
 	</div>
-	<div class:hidden={activeScreen == 'Instrukcja' || activeScreen == 'Rozpoczęcie'}>
+	<div
+		class="flex h-full flex-col justify-center"
+		class:hidden={activeScreen == 'Instrukcja' || activeScreen == 'Rozpoczęcie'}
+	>
 		<GameProgressScreen />
 	</div>
 </section>
