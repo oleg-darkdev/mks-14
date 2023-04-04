868c21795ea949350a222e93d33e16a104f00235
DarkDev <darkdev_brand@riseup.net>
2023-04-04 03:57:44 +0300

    upd text, refactoring structure

--- src/lib/widgets/ui/StartScreen.svelte
+++ src/lib/widgets/ui/StartScreen.svelte
@@ -35,7 +35,7 @@
 		},
 		{
 			title: 'Przygotowanie kart eksperymentów',
-			desc: 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.',
+			desc: 'Откройте 3 эксперимента из верха колоды экспериментов. Для вашего уровня сложности, на конец игры вам нужно выполнить экспериментов указанных ниже',
 			img: ''
 		},
 		{
@@ -49,61 +49,59 @@
 	export let activeScreen;
 </script>
 
-<section class="flex min-h-screen flex-col items-center  justify-center pt-10 pb-40">
-	{#if step == 0}
+{#if step == 0}
+	<TextBlock
+		title="Witamy serdecznie na MKC-14"
+		desc="Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups."
+	>
+		<HorisontalImage slot="img" img="./images/hero.png" alt=" Welcome banner" />
+		<StepsBtns slot="btn" bind:step />
+	</TextBlock>
+{:else if step >= 0 && step <= 8}
+	{#if step == 2}
+		<StepsList steps={gameSteps}>
+			<StepsBtns bind:step />
+		</StepsList>
+	{:else if step == 3}
+		<RolesSetup bind:step />
+	{:else if step == 4}
 		<TextBlock
-			title="Witamy serdecznie na MKC-14"
-			desc="Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups."
+			title="Przygotowanie mapy "
+			desc="W każdej grze kolejność lokalizacji (z 2 po 6) na na stacji MKS-14 zmienia się losowo."
 		>
-			<HorisontalImage slot="img" img="./images/hero.png" alt=" Welcome banner" />
 			<StepsBtns slot="btn" bind:step />
 		</TextBlock>
-	{:else if step >= 0 && step <= 8}
-		{#if step == 2}
-			<StepsList steps={gameSteps}>
-				<StepsBtns bind:step />
-			</StepsList>
-		{:else if step == 3}
-			<RolesSetup bind:step />
-		{:else if step == 4}
-			<TextBlock
-				title="Przygotowanie mapy "
-				desc="W każdej grze kolejność lokalizacji (z 2 po 6) na na stacji MKS-14 zmienia się losowo."
-			>
-				<StepsBtns slot="btn" bind:step />
-			</TextBlock>
-			<MapGenerator />
-		{:else if step == 5}
-			<LevelSecelect bind:step />
-		{:else if step == 6}
-			<TextBlock
-				title="'Dostarczanie surowców do ISS"
-				desc="Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups."
-			>
-				<HorisontalImage slot="img" img="" alt="Science banner" />
-				<StepsBtns slot="btn" bind:step />
-			</TextBlock>
-		{:else if step == 7}
-			<TextBlock
-				title="Przygotowanie kart eksperymentów"
-				desc="Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups."
-			>
-				<HorisontalImage slot="img" img="/images/manual/science_bg.png" alt="Science banner" />
-				<StepsBtns slot="btn" bind:step />
-			</TextBlock>
-		{/if}
-	{:else}
-		<FinalStepOnScreen
-			title="Yay, prawie skończyliśmy przygotowania"
-			desc={[
-				'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.',
-				'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.'
-			]}
-			btnText="Rozpocząć grę"
-			on:click={() => {
-				activeScreen = 'Rozgrywka';
-				step = 1;
-			}}
-		/>
+		<MapGenerator />
+	{:else if step == 5}
+		<LevelSecelect bind:step />
+	{:else if step == 6}
+		<TextBlock
+			title="'Dostarczanie surowców do ISS"
+			desc="Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups."
+		>
+			<HorisontalImage slot="img" img="" alt="Science banner" />
+			<StepsBtns slot="btn" bind:step />
+		</TextBlock>
+	{:else if step == 7}
+		<TextBlock
+			title="Przygotowanie kart eksperymentów"
+			desc="Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups."
+		>
+			<HorisontalImage slot="img" img="/images/manual/science_bg.png" alt="Science banner" />
+			<StepsBtns slot="btn" bind:step />
+		</TextBlock>
 	{/if}
-</section>
+{:else}
+	<FinalStepOnScreen
+		title="Yay, prawie skończyliśmy przygotowania"
+		desc={[
+			'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.',
+			'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.'
+		]}
+		btnText="Rozpocząć grę"
+		on:click={() => {
+			activeScreen = 'Rozgrywka';
+			step = 1;
+		}}
+	/>
+{/if}
