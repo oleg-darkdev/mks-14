62b1919a4aaca1158f9c92826ffddfa375bbe032
DarkDev <darkdev_brand@riseup.net>
2023-04-04 03:58:35 +0300

    fix width

--- src/lib/entities/ui/landing/ShortTeamMemberCard.svelte
+++ src/lib/entities/ui/landing/ShortTeamMemberCard.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div
-	class="mx-1 my-12 flex h-32 w-full flex-col justify-center rounded-md border-2   border-violet-700 bg-gray-700 px-2 pb-4 text-center text-violet-500 shadow-md shadow-violet-700 md:mx-6 md:w-64 md:px-6 lg:mx-6  lg:w-60 lg:px-8 xl:mx-6 xl:w-40 xl:px-8"
+	class="mx-1 my-12 flex h-32 w-60  flex-col justify-center rounded-md border-2  border-violet-700 bg-gray-800  px-2 pb-4 text-center text-violet-500 shadow-md shadow-violet-700 xl:mx-6  "
 >
 	<img
 		alt={member.fullName}
