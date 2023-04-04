906fcf9f55862a847c06a0dba43b2a4d36fe9fbb
DarkDev <darkdev_brand@riseup.net>
2023-04-04 03:56:35 +0300

    alfa v.

--- src/routes/app/experiments/[slug]/+page.svelte
+++ src/routes/app/experiments/[slug]/+page.svelte
@@ -1,2 +1,12 @@
 <script>
+	import { Article } from '../../../../lib/shared/';
+	import { page } from '$app/stores';
+	import { getCurrentUrl, getLocationFromUrl, identifySelectedData } from '../../../../lib/shared/';
+
+	const locationValue = getLocationFromUrl(getCurrentUrl(window)),
+		textArray = identifySelectedData(locationValue),
+		getThisArticleText = (textArray) => textArray.data[$page.params.slug - 1];
+
 </script>
+
+<Article manualArticle={getThisArticleText(textArray)} />
