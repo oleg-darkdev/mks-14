62d6f2e6c192c4ff0b90ffa99626d4233c48c560
DarkDev <darkdev_brand@riseup.net>
2023-04-04 03:59:44 +0300

    alfa v.

---
+++ src/lib/shared/data/toc/extensionsToc.js
@@ -0,0 +1,71 @@
+const TOC = {
+	link: '/app/extensions/',
+	img: '/images/manual/toc/toc_extensions.png',
+
+	data: [
+		{
+			title: 'extension: USA',
+			id: '1',
+			img: '/images/manual/toc/toc_synopsis.png',
+			shortDesc:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+			fullDesc: [
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+			]
+		},
+
+		{
+			title: 'extension: Europe',
+			id: '2',
+			img: '/images/manual/toc/toc_synopsis.png',
+			shortDesc:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+			fullDesc: [
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+			]
+		},
+		{
+			title: 'extension: Asia',
+			id: '3',
+			img: '/images/manual/toc/toc_synopsis.png',
+			shortDesc:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+			fullDesc: [
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+			]
+		},
+		{
+			title: 'extension: USSR',
+			id: '4',
+			img: '/images/manual/toc/toc_synopsis.png',
+			shortDesc:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+			fullDesc: [
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+			]
+		},
+		{
+			title: 'extension: France',
+			id: '5',
+			img: '/images/manual/toc/toc_synopsis.png',
+			shortDesc:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+			fullDesc: [
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+			]
+		},
+		{
+			title: 'extension: Belgium',
+			id: '6',
+			img: '/images/manual/toc/toc_synopsis.png',
+
+			shortDesc:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+			fullDesc: [
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+			]
+		}
+	]
+};
+
+export default TOC;
