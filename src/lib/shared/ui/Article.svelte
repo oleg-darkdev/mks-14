1281b18280a6ce98eb4d1f7696dde614c31f48f1
DarkDev <darkdev_brand@riseup.net>
2023-04-05 03:12:40 +0300

    add resources block

--- src/lib/shared/ui/Article.svelte
+++ src/lib/shared/ui/Article.svelte
@@ -31,9 +31,33 @@
 				class="m-4 mx-auto -mt-16 space-y-6 border-2 border-violet-700 bg-gray-900 p-6 pb-12 shadow-md shadow-violet-700 sm:mx-12 sm:px-10 lg:max-w-2xl lg:rounded-md"
 			>
 				<div class="space-y-2">
-					<h1 class="inline-block text-2xl font-semibold sm:text-3xl">
-						#{manualArticle.id} {manualArticle.title}
+					<h1 class="inline-block text-4xl font-semibold">
+						#{manualArticle.id}
+						{manualArticle.title}
 					</h1>
+				</div>
+				<div class:hidden={!manualArticle.resources.length}>
+					<h2 class="text-2xl font-semibold">Required Resources</h2>
+
+					<div class="flex flex-row flex-wrap justify-center">
+						{#each manualArticle.resources as resources}
+							<div
+								class="relative m-1 h-32 w-32 rounded-lg border border-violet-700 shadow-md shadow-violet-700"
+								class:hidden={resources.amount == 0}
+							>
+								<img
+									style="opacity: 0.4;"
+									class="rounded-lg"
+									src={resources.img}
+									alt={resources.title}
+									srcset=""
+								/>
+								<span class="absolute inset-x-10 inset-y-3 text-8xl text-gray-100"
+									>{resources.amount}</span
+								>
+							</div>
+						{/each}
+					</div>
 				</div>
 				<div class="text-gray-100">
 					{#each manualArticle.fullDesc as fullDesc}
@@ -45,8 +69,8 @@
 	</article>
 </section>
 
-<section class="flex flex-row justify-between bg-gray-900 p-2 pb-4 text-gray-50 mb-6">
+<section class="mb-6 flex flex-row justify-between bg-gray-900 p-2 pb-4 text-gray-50">
 	{#each mainTocData.data as recommendation}
-		<RecommendationBlock {recommendation} />
+		<RecommendationBlock link='/app/manual' {recommendation} />
 	{/each}
 </section>
