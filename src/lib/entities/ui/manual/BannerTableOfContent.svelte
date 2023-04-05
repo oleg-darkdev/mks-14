c3d57b60f57af0909a77d314b31561c17e725472
DarkDev <darkdev_brand@riseup.net>
2023-04-05 03:13:21 +0300

    upd styles

--- src/lib/entities/ui/manual/BannerTableOfContent.svelte
+++ src/lib/entities/ui/manual/BannerTableOfContent.svelte
@@ -10,12 +10,12 @@
 		alt="{page} banner"
 		class="h-64 w-full  bg-violet-700 object-cover sm:h-96 lg:col-span-7"
 	/>
-	<div class="space-y-2 p-6 lg:col-span-5">
+	<div class="px-auto space-y-2 p-6 lg:col-span-5">
 		<h3 class="text-2xl font-semibold  sm:text-4xl">
-			Opis  {page} dostępnych w grze
+			Opis {page} dostępnych w grze
 		</h3>
 		<ul class="grid grid-cols-2 gap-2">
-			{#each toc.data as item}
+			{#each toc.data.slice(0, 8) as item}
 				<li class="-mt-2">
 					<a class="hover:text-violet-700" href="{toc.link}{item.id}"
 						><span class="text-violet-700">#{item.id}</span> {item.title}</a
@@ -23,5 +23,6 @@
 				</li>
 			{/each}
 		</ul>
+		<!-- <a href="#all" class="btn-md btn  btn-wide bg-violet-700">show all</a> -->
 	</div>
 </div>
