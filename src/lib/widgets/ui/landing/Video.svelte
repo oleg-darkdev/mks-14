c49e46803f9faf72cdc804ae9fa496d41b0c41f2
DarkDev <darkdev_brand@riseup.net>
2023-04-05 03:11:43 +0300

    alfa v.

---
+++ src/lib/widgets/ui/landing/Video.svelte
@@ -0,0 +1,23 @@
+<section class="grid gap-6 px-4 md:grid-cols-2">
+	<div
+		class="embed-responsive embed-responsive-16by9 relative w-full  overflow-hidden rounded border-2 border-violet-700 shadow-md shadow-violet-700"
+		style="padding-top: 56.25%"
+	>
+		<iframe
+			class="embed-responsive-item absolute top-0 right-0 bottom-0 left-0 h-full w-full rounded"
+			src="https://www.youtube.com/embed/v64KOxKVLVg"
+			allowfullscreen
+		/>
+	</div>
+
+	<div
+		class="embed-responsive embed-responsive-16by9 relative w-full overflow-hidden rounded border-2 border-violet-700 shadow-md shadow-violet-700"
+		style="padding-top: 56.25%"
+	>
+		<iframe
+			class="embed-responsive-item absolute top-0 right-0 bottom-0 left-0 h-full w-full rounded"
+			src="https://player.vimeo.com/video/137857207"
+			allowfullscreen
+		/>
+	</div>
+</section>
