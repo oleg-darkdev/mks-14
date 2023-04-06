9259548fd6ed1aadc65e6af8a4478ab00e60990b
DarkDev <darkdev_brand@riseup.net>
2023-04-06 07:35:40 +0300

    refactoring resources block

---
+++ src/lib/entities/ui/manual/SquareImagesWrapper.svelte
@@ -0,0 +1,25 @@
+<script>
+  export let data;
+</script>
+
+<div class:hidden={!data.resources.length}>
+	<h2 class="text-2xl font-semibold">Required Resources</h2>
+
+	<div class="flex flex-row flex-wrap justify-center">
+		{#each data.resources as resources}
+			<div
+				class="relative m-1 h-32 w-32 rounded-lg border border-violet-700 shadow-md shadow-violet-700"
+				class:hidden={resources.amount == 0}
+			>
+				<img
+					style="opacity: 0.4;"
+					class="rounded-lg"
+					src={resources.img}
+					alt={resources.title}
+					srcset=""
+				/>
+				<span class="absolute inset-x-10 inset-y-3 text-8xl text-gray-100">{resources.amount}</span>
+			</div>
+		{/each}
+	</div>
+</div>
--- src/lib/shared/ui/Article.svelte
+++ src/lib/shared/ui/Article.svelte
@@ -36,29 +36,7 @@
 						{manualArticle.title}
 					</h1>
 				</div>
-				<div class:hidden={!manualArticle.resources.length}>
-					<h2 class="text-2xl font-semibold">Required Resources</h2>
-
-					<div class="flex flex-row flex-wrap justify-center">
-						{#each manualArticle.resources as resources}
-							<div
-								class="relative m-1 h-32 w-32 rounded-lg border border-violet-700 shadow-md shadow-violet-700"
-								class:hidden={resources.amount == 0}
-							>
-								<img
-									style="opacity: 0.4;"
-									class="rounded-lg"
-									src={resources.img}
-									alt={resources.title}
-									srcset=""
-								/>
-								<span class="absolute inset-x-10 inset-y-3 text-8xl text-gray-100"
-									>{resources.amount}</span
-								>
-							</div>
-						{/each}
-					</div>
-				</div>
+				<slot/>
 				<div class="text-gray-100">
 					{#each manualArticle.fullDesc as fullDesc}
 						<p>{fullDesc}</p>
