d0cfc01cb664f298b9de7d50e3edbe8e16453adb
DarkDev <darkdev_brand@riseup.net>
2023-04-06 07:51:20 +0300

    alfa v.

---
+++ src/routes/synopsis/+page.svelte
@@ -0,0 +1,56 @@
+<script>
+	import {
+		HeroLanding,
+		StatsLanding,
+		FeaturesLanding,
+		GalleryLanding,
+		AppLanding,
+		StepsLanding,
+		ReviewsLanding,
+		TeamLanding,
+    VideoLanding,
+		FooterLanding,
+    GameKitLanding
+	} from '../../lib/widgets/';
+
+
+</script>
+
+
+<HeroLanding />
+
+<VideoLanding />
+
+<a name="gallery" />
+<GalleryLanding />
+
+<a name="app" />
+<AppLanding />
+
+
+
+<a name="updates" />
+<StepsLanding />
+
+<GameKitLanding />
+
+<!-- pricing -->
+<!-- <section class="py-6 bg-gray-900 text-gray-50">
+	<div class="container mx-auto flex flex-col items-center justify-center max-w-lg p-4 lg:max-w-full sm:p-10 lg:flex-row">
+		<div class="flex flex-col items-center justify-center flex-1 p-4 pb-8 sm:p-8 lg:p-16 bg-gray-900">
+			<span class="text-sm">Basic</span>
+			<p class="text-5xl font-bold text-center">39€</p>
+			<p class="font-semibold text-center">Nemo deserunt possimus quo provident recusandae! Dolores qui architecto omnis pariatur eos voluptatibus sequi cum, non nesciunt aspernatur a?</p>
+			<button class="px-8 py-3 mt-6 text-lg font-semibold border rounded sm:mt-12 border-gray-700">Sign up</button>
+		</div>
+		<div class="flex flex-col items-center justify-center flex-1 p-4 pb-8 text-center rounded-md sm:p-8 lg:p-16 bg-violet-400 text-gray-900">
+			<span class="text-sm font-semibold">Advanced</span>
+			<p class="text-5xl font-bold">89€</p>
+			<p class="font-semibold">Nemo deserunt possimus quo provident recusandae! Dolores qui architecto omnis pariatur eos voluptatibus sequi cum, non nesciunt aspernatur a?</p>
+			<button class="px-8 py-3 mt-6 text-lg font-semibold rounded sm:mt-12 bg-gray-900 text-gray-50">Sign up</button>
+		</div>
+	</div>
+</section> -->
+
+
+<FooterLanding />
