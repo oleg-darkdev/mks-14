2b6abe1b73bdc05fc218dc1bfeaa7b91f940566d
DarkDev <darkdev_brand@riseup.net>
2023-04-10 02:49:16 +0300

    alfa v.

---
+++ src/lib/entities/ui/landing/ShortListOfExtensions.svelte
@@ -0,0 +1,41 @@
+<script>
+  export let data, link;
+</script>
+
+<div
+	class="max-w-2xl m-1  rounded-lg border-2 border-violet-700 bg-gray-900 p-6 p-4 shadow-md  shadow shadow-violet-700 sm:p-8"
+>
+	<div class="mb-4 flex items-center justify-between">
+		<h5 class="text-xl font-bold leading-none text-gray-900 dark:text-white">{data.title}</h5>
+		<a href='{link}{data.link}' class="text-sm font-medium text-violet-700 hover:underline dark:text-blue-500">
+			View all
+		</a>
+	</div>
+	<div class="flow-root">
+		<ul class="divide-y divide-violet-700 ">
+			{#each data ? data.data : data.data.splice(0, 5) as category}
+				<li class="py-3 sm:py-4">
+					<div class="flex items-center space-x-4">
+						<div class="flex-shrink-0">
+							<img class="h-20 w-20 rounded-lg" src={category.img} alt={category.title} />
+						</div>
+						<div class="min-w-0 flex-1">
+							<p class="truncate text-sm font-medium text-gray-900 dark:text-white">
+								{category.title}
+							</p>
+							<p class="truncate text-sm text-gray-500 dark:text-gray-400">
+								{category.shortDesc}
+							</p>
+						</div>
+
+						<div
+							class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white"
+						>
+							{category.experimentsList.length}
+						</div>
+					</div>
+				</li>
+			{/each}
+		</ul>
+	</div>
+</div>
---
+++ src/lib/entities/ui/landing/TwoColumnTable.svelte
@@ -0,0 +1,38 @@
+<script>
+  export let data;
+  
+	let showAll = false;
+</script>
+
+<div class="max-w-2xl  rounded-lg border-2 border-violet-700 p-6 shadow-md shadow-violet-700">
+	<h3 class="text-xl font-bold text-violet-700 sm:text-2xl">Box set</h3>
+	<p class="mt-2 text-gray-400">
+		Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+	</p>
+
+	<div
+		class="mt-4 mb-6 overflow-x-auto rounded-lg border border-violet-700  shadow-md  shadow-violet-700"
+	>
+		<table class="w-full table-auto text-left text-sm">
+			<thead class="border-b border-b-violet-700 bg-violet-700  font-medium text-gray-100">
+				<tr>
+					<th class="py-3 px-6">title</th>
+					<th class="py-3 px-6">amount</th>
+				</tr>
+			</thead>
+			<tbody class="divide-y divide-violet-700 text-violet-700 ">
+				{#each showAll ? data : data.splice(0, 3) as item}
+					<tr class="hover:bg-gray-100 ">
+						<td class="whitespace-nowrap px-6 py-4">{item.title}</td>
+						<td class="whitespace-nowrap px-6 py-4">{item.amount}</td>
+					</tr>
+				{/each}
+			</tbody>
+		</table>
+	</div>
+	<div class="mb-12 flex items-center justify-center">
+		<button on:click={() => (showAll = !showAll)} class="btn-wide btn bg-violet-700"
+			>Show all</button
+		>
+	</div>
+</div>
