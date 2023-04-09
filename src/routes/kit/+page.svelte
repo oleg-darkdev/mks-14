55d1314ed780e5701c1a429e4323b54849eeddde
DarkDev <darkdev_brand@riseup.net>
2023-04-10 02:46:29 +0300

    alfa v.

---
+++ src/routes/kit/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { FullGameKit, AppLanding, FooterLanding, GalleryLanding } from '../../lib/widgets';
+</script>
+
+<!-- <a name="about" />
+<StatsLanding /> -->
+
+<!-- <VideoLanding /> -->
+
+<!-- <GalleryLanding /> -->
+<AppLanding />
+
+<FullGameKit />
+
+<!-- pricing -->
+
+<FooterLanding />
