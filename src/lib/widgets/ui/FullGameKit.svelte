296fb7256151d8a3c389a5c9cb0aee9647c9416e
DarkDev <darkdev_brand@riseup.net>
2023-04-10 02:48:25 +0300

    refactoring

---
+++ src/lib/widgets/ui/FullGameKit.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { ShortListOfExtensions, TwoColumnTable } from '../../entities';
+	import {
+		gameComplect,
+		extensionsExperimentsToc,
+	} from '../../shared';
+</script>
+
+<section class="mx-auto mt-12 flex flex-col items-center  md:px-8">
+	<TwoColumnTable data={gameComplect} />
+
+	<div
+		class="mt-12 mb-12 flex flex-row flex-wrap items-center justify-between rounded-lg border-2 border-violet-700 p-6 shadow-md shadow-violet-700"
+	>
+		<ShortListOfExtensions data={extensionsExperimentsToc} link="" />
+		<ShortListOfExtensions data={extensionsExperimentsToc} link="" />
+	</div>
+</section>
