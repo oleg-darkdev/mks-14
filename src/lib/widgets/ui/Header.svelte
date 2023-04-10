27eef6ccf445ff4e1d070278bd309e25c6e738b0
DarkDev <darkdev_brand@riseup.net>
2023-04-10 22:05:39 +0300

    adaptive for mobile

--- src/lib/widgets/ui/Header.svelte
+++ src/lib/widgets/ui/Header.svelte
@@ -3,18 +3,27 @@
 	export let leftSideLinks, rightSideLinks;
 </script>
 
-<header class="bg-gray-900 p-4 text-gray-100">
-	<div class="container mx-auto flex h-16 justify-center md:space-x-8">
+<header class="bg-violet-700 p-2 text-gray-100 lg:p-4">
+	<div class="nav-wrap container mx-auto flex h-auto items-center justify-center md:space-x-8">
 		<ul class="hidden items-stretch space-x-3 md:flex">
 			{#each leftSideLinks as link}<li class="flex">
 					<HeaderLink {link} />
 				</li>
 			{/each}
 		</ul>
-		<div class="h-20 w-20">
-			<SmallLogoLink />
-		</div>
+		<SmallLogoLink />
 		<ul class="hidden items-stretch space-x-3 md:flex">
+			{#each rightSideLinks as link}<li class="flex">
+					<HeaderLink {link} />
+				</li>
+			{/each}
+		</ul>
+
+		<ul class="mobile-nav container grid grid-cols-6 gap-1 overflow-hidden xl:grid-cols-3">
+			{#each leftSideLinks as link}<li class="flex">
+					<HeaderLink {link} />
+				</li>
+			{/each}
 			{#each rightSideLinks as link}<li class="flex">
 					<HeaderLink {link} />
 				</li>
@@ -22,3 +31,17 @@
 		</ul>
 	</div>
 </header>
+
+<style>
+	@media (max-width: 560px) {
+		.nav-wrap {
+			display: flex;
+			flex-direction: column;
+		}
+	}
+	@media (min-width: 560px) {
+		.mobile-nav {
+			display: none;
+		}
+	}
+</style>
