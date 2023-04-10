04465a2ffa6341c52367bebfaadafeb2ee398a06
DarkDev <darkdev_brand@riseup.net>
2023-04-10 22:03:09 +0300

    add hover effect & image

--- src/lib/shared/ui/HeaderLink.svelte
+++ src/lib/shared/ui/HeaderLink.svelte
@@ -2,8 +2,12 @@
 	export let link;
 </script>
 
-<a
-	rel="noopener noreferrer"
-	href={link.link}
-	class="-mb-1 flex items-center border-b-2 border-transparent px-4">{link.text}</a
->
+<div class="flex flex-col items-center  ">
+	<img src="/images/header/{link.img}" class="h-10 w-10" />
+	<a
+		rel="noopener noreferrer"
+		href={link.link}
+		class="-mb-1 flex items-center border-b-2 border-transparent px-1 text-sm  hover:border-b-violet-900 hover:text-violet-900"
+		>{link.text}</a
+	>
+</div>
