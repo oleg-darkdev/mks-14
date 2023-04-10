ac2d776f1a5b8c53e32003b751877461f92708fb
DarkDev <darkdev_brand@riseup.net>
2023-04-10 22:05:23 +0300

    upd text

--- src/lib/widgets/ui/landing/Features.svelte
+++ src/lib/widgets/ui/landing/Features.svelte
@@ -60,24 +60,13 @@
 	<div
 		class="container mx-auto max-w-xl space-y-24 p-6 py-2 md:py-10 lg:max-w-7xl lg:py-12 lg:px-2 xl:py-12"
 	>
-		<div class="pt-6">
-			<h2
-				class="text-center text-2xl font-bold tracking-tight text-gray-50 md:text-3xl lg:text-3xl xl:text-3xl "
-			>
-				MKS-14 edukacyjna gra planszowa
-			</h2>
-			<p class="mx-auto mt-4 max-w-3xl text-center text-xl text-gray-400">
-				Quando cetero his ne, eum admodum sapientem ut.
-			</p>
-		</div>
 		<div class="grid lg:grid-cols-2 lg:items-center lg:gap-8">
 			<div>
 				<h3 class="text-2xl font-bold tracking-tight text-gray-50 sm:text-3xl">
-					Opis gry planszowej
+					Opis gry planszowej MKS-14
 				</h3>
 				<p class="mt-3 text-lg text-gray-400">
-					Ne dicta praesent ocurreret has, diam theophrastus at pro. Eos etiam regione ut, persius
-					eripuit quo id. Sit te euismod tacimates.
+					edukacyjna kolekcjonerska gra o międzynarodowej stacji kosmicznej
 				</p>
 				<div class="mt-12 space-y-12">
 					{#each featuresBoardgame as text}
@@ -94,7 +83,7 @@
 			<div class="grid lg:grid-cols-2 lg:items-center lg:gap-8">
 				<div class="lg:col-start-2">
 					<h3 class="text-2xl font-bold tracking-tight text-gray-50 sm:text-3xl">
-						Aplikacja webowa
+						Opis aplikacji webowej  MKS-14
 					</h3>
 					<p class="mt-3 text-lg text-gray-400">
 						Per odio fabellas consulatu cu. Utroque detracto mel ea, quo te latine theophrastus. Ea
