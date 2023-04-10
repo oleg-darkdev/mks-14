1121d5f15723ffef78685a9b46ce0eac85c614eb
DarkDev <darkdev_brand@riseup.net>
2023-04-10 22:02:02 +0300

    upd text

--- src/lib/shared/data/getGame.js
+++ src/lib/shared/data/getGame.js
@@ -1,14 +1,11 @@
 const steps = [
 	{
 		title: 'Pobrać za darmo',
-		subtitle: 'Bezpłatne pobieranie plików pdf dla PnP',
-		desc: [
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-		],
-		img: '',
+		subtitle: 'Bezpłatne pobieranie dla PnP',
+		desc: 'Pliki do wydrukowania gry planszowej na domowej drukarce - do pobrania na githubie.',
+		link: 'https://github.com/oleg-darkdev/mks-14',
 		id: '1'
-	},
+	}
 	// {
 	// 	title: 'Lorem ipsum dolor sit amet.',
 	// 	subtitle: 'dolor sit amet',
