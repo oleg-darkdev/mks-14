d2049ef5e37e4d64e4f0fb95d44a5c2fcdd0842b
DarkDev <darkdev_brand@riseup.net>
2023-04-10 22:05:48 +0300

    upd styles

--- src/lib/shared/ui/SmallLogoLink.svelte
+++ src/lib/shared/ui/SmallLogoLink.svelte
@@ -1,3 +1,3 @@
-<a rel="noopener noreferrer" href="/" aria-label="Go to homepage" class="flex items-center p-2">
-	<img src="/images/logo.svg" alt="mks-14 logo" class="w-full h-full" srcset="" />
+<a rel="noopener noreferrer" href="/" aria-label="Go to homepage" class="flex items-center">
+	<img src="/images/logo.svg" alt="mks-14 logo" class="w-40" srcset="" />
 </a>
