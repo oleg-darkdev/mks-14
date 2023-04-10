d0711c0258a785ecc6c45d94eff9caa9e916bd7c
DarkDev <darkdev_brand@riseup.net>
2023-04-10 22:04:01 +0300

    alfa v.

---
+++ src/lib/widgets/ui/landing/Testimonial.svelte
@@ -0,0 +1,27 @@
+<script>
+</script>
+
+<section class="mb-6 bg-gray-900 text-gray-100">
+	<div class="container mx-auto flex flex-col items-center space-y-6 p-4 md:p-8">
+		<img src="/images/landing/testimonial.svg" class="h-40 w-40" alt="testimonial" />
+
+		<p
+			class="hover:text-violet-700 px-6 py-2 text-center text-2xl font-semibold text-gray-300 sm:text-3xl sm:font-bold md:text-4xl lg:max-w-2xl xl:max-w-4xl"
+		>
+			"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+			been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
+			galley of type and scrambled it to make a type specimen book."
+		</p>
+		<div class="flex justify-center space-x-2">
+			<img
+				src="https://source.unsplash.com/80x80/?portrait?1"
+				alt=""
+				class="h-20 w-20 rounded-md bg-cover bg-center bg-gray-500 bg-gray-700"
+			/>
+			<div class="text-3xl ">
+				<p class="leading-tight">Lorem impsum</p>
+				<p class="text-sm leading-tight text-gray-300">Founder, Company</p>
+			</div>
+		</div>
+	</div>
+</section>
