28f57056218429ec27ea7e7bb8b593e6915b87f4
DarkDev <darkdev_brand@riseup.net>
2023-04-12 02:52:47 +0300

    upd text

--- src/lib/widgets/ui/StartScreen.svelte
+++ src/lib/widgets/ui/StartScreen.svelte
@@ -98,7 +98,6 @@
 			'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.',
 			'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.'
 		]}
-		btnText="Rozpocząć grę"
 		on:click={() => {
 			activeScreen = 'Rozgrywka';
 			step = 1;
