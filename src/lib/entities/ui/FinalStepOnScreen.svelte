94f022165102b3e10ac9d2ee84ada5fc0aabfcac
DarkDev <darkdev_brand@riseup.net>
2023-04-12 02:51:45 +0300

    refactoring & exports

--- src/lib/entities/ui/FinalStepOnScreen.svelte
+++ src/lib/entities/ui/FinalStepOnScreen.svelte
@@ -1,28 +1,27 @@
 <script>
 	import { HorisontalImage } from '../../shared';
 
-	export let desc, title, btnText;
+	export let desc, title;
 </script>
 
-<div class="rounded-lg border-2 border-violet-700 shadow-md shadow-violet-700 ">
+<div class="mx-2 rounded-lg border-2 border-violet-700 shadow-md shadow-violet-700 ">
 	<HorisontalImage slot="img" img="./images/hero.png" alt="Hello banner" />
 
 	<div class="mx-auto max-w-screen-xl py-6 px-4 md:px-8 md:text-center">
 		<div class="max-w-xl md:mx-auto">
-			<h3 class="text-3xl font-semibold text-gray-300 sm:text-4xl">{title}</h3>
+			<h3 class=" text-2xl font-semibold text-gray-300 md:text-3xl lg:text-3xl xl:text-3xl ">
+				{title}
+			</h3>
 			{#each desc as text}
 				<p class="mt-3 text-gray-300">
 					{text}
 				</p>
 			{/each}
 		</div>
-		<div class="mt-4 flex items-center gap-3 md:justify-center">
-			<button
-				on:click
-				class="m-2 inline-block rounded-lg bg-violet-700 py-2 px-4 font-medium text-white shadow-md duration-150 hover:bg-gray-700 hover:shadow-none active:bg-gray-900"
+		<div class="mt-4 flex items-center justify-center">
+			<button class="btn-lg  btn mx-6 mt-4 max-w-2xl bg-violet-700" on:click
+				>Rozpocząć grę</button
 			>
-				{btnText}
-			</button>
 		</div>
 	</div>
 </div>
