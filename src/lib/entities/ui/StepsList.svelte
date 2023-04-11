d08b7266002f7f1e103743fda226c90258edc559
DarkDev <darkdev_brand@riseup.net>
2023-04-12 02:53:11 +0300

    fix classes for mobile

--- src/lib/entities/ui/StepsList.svelte
+++ src/lib/entities/ui/StepsList.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <div
-	class="mx-auto border border-violet-700 shadow-md shadow-violet-700 rounded-lg px-4 py-4 sm:max-w-xl md:max-w-full md:px-24 lg:max-w-screen-xl lg:px-8 lg:py-4"
+	class="mx-2 border border-violet-700 shadow-md shadow-violet-700 rounded-lg px-4 py-4 sm:max-w-xl md:max-w-full md:px-24 lg:max-w-screen-xl lg:px-8 lg:py-4"
 >
   <h3 class="text-center text-2xl font-semibold text-gray-100 sm:text-4xl">Kroki przygotowania do gry</h3>
 	<div class="mx-auto grid max-w-2xl">
