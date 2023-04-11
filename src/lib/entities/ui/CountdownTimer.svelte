fabcd22ca54bbb96f79d0375a96f07a80a721090
DarkDev <darkdev_brand@riseup.net>
2023-04-12 02:51:11 +0300

    refactoring logic & export

--- src/lib/entities/ui/CountdownTimer.svelte
+++ src/lib/entities/ui/CountdownTimer.svelte
@@ -1,21 +1,24 @@
 <script>
 	import { tweened } from 'svelte/motion';
-
-	export let timeToEnd;
-	$: timer = tweened(timeToEnd * 60);
-
-	setInterval(() => {
-		if ($timer > 0) $timer--;
-	}, 1000);
+	$: timer = tweened(time);
 
 	$: minutes = Math.floor($timer / 60);
 	$: minname = minutes > 1 ? 'mins' : 'min';
 	$: seconds = Math.floor($timer - minutes * 60);
+
+	const interval = setInterval(() => {
+		if ($timer > 0) $timer--;
+		if ($timer == 0) {
+			clearInterval(interval);
+			showMsg = true;
+		}
+	}, 1000);
+
+  export let showMsg, time;
 </script>
 
 <div class="flex flex-row flex-wrap items-center justify-center ">
-	<span class="font-mono align-center countdown text-7xl">
-		<span style="--value:{minutes};" />:
-		<span style="--value:{seconds};" />
+	<span style="font-size: 100px;" class="font-mono align-center countdown font-black ">
+		<span style="--value:{minutes};" />:<span style="--value:{seconds};" />
 	</span>
 </div>
