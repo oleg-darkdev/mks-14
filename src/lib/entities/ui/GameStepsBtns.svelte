be00df10f833cd0b878bd4e4aae7a0442d171ea4
DarkDev <darkdev_brand@riseup.net>
2023-04-12 02:51:40 +0300

    refactoring & exports

--- src/lib/entities/ui/GameStepsBtns.svelte
+++ src/lib/entities/ui/GameStepsBtns.svelte
@@ -1,17 +1,11 @@
 <script>
-	const stepIncrease = () => {
-		timeToEnd--;
-		step++;
-	};
-
-	export let step, timeToEnd;
+	export let step, showMsg;
 </script>
 
 <div class="flex w-full flex-row flex-wrap justify-center">
 	<button
-		on:click={() => stepIncrease()}
-		class="m-2 inline-block rounded-lg bg-violet-700 py-2 px-4 font-medium text-white shadow-md duration-150 hover:bg-gray-100 hover:bg-gray-700 hover:text-gray-900 hover:shadow-none active:bg-gray-900"
+		on:click
+		class="btn-lg  btn mx-6 mt-4 max-w-2xl {showMsg ? 'bg-gray-900' : 'bg-violet-700'}"
+		>{step < 1 ? 'Rozpocząć grę' : 'Następna runda'}</button
 	>
-		Next
-	</button>
 </div>
