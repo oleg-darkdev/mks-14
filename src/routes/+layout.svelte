bf575c16fbdf00c79c359f634a926b1fa27b56fa
DarkDev <darkdev_brand@riseup.net>
2023-04-12 20:00:55 +0300

    add font for numbers, intial animation lib

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,28 @@
 <script>
 	import '../app.css';
-	// import 'aos/dist/aos.css';
-	// import AOS from 'aos';
-	// import { onMount } from 'svelte';
-	// onMount(() => {
-	// 	AOS.init();
-	// });
+	import 'aos/dist/aos.css';
+	import AOS from 'aos';
+	import { onMount } from 'svelte';
+	onMount(() => {
+		AOS.init();
+	});
 </script>
 
 <slot />
 
 <style>
+  	:global(.font-numbers) {
+		font-family: 'Numbers';
+		line-height: 1.2;
+	}
+
+	@font-face {
+		font-family: 'Numbers';
+		font-style: normal;
+		font-weight: 500;
+
+		src: local(''), /* Super Modern Browsers */ url('/fonts/numbers.woff') format('woff'),
+			/* Modern Browsers */ url('/fonts/numbers.ttf') format('truetype'),
+			/* Safari, Android, iOS */ url('/fonts/numbers.svg') format('svg'); /* Legacy iOS */
+	}
 </style>
