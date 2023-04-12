18e79891860eace11a31641dec8a3b969a741377
DarkDev <darkdev_brand@riseup.net>
2023-04-13 01:09:35 +0300

    add price values, upd extensions data

--- src/lib/shared/data/toc/extensions/persons/austria.js
+++ src/lib/shared/data/toc/extensions/persons/austria.js
@@ -3,7 +3,23 @@ const TOC = {
 	link: '/app/experiments/',
 	img: '',
 
-	data: []
+	data: [
+		{
+			title: 'Lorem',
+			country: 'lorem',
+			year: `2002`,
+			id: 1,
+			img: '/images/manual/toc/persons/',
+			sources: [
+				{
+					title: 'NASA',
+					link: 'https://www.nasa.gov/astronauts/biographies/fyodor-yurchikhin/biography'
+				}
+			],
+			shortDesc: 'lorem',
+			fullDesc: ['lorem']
+		}
+	]
 };
 
 export default TOC;
--- src/lib/shared/data/toc/extensions/persons/usa.js
+++ src/lib/shared/data/toc/extensions/persons/usa.js
@@ -4,7 +4,21 @@ const TOC = {
 	img: '',
 
 	data: [
-
+		{
+			title: 'Lorem',
+			country: 'lorem',
+			year: `2002`,
+			id: 1,
+			img: '/images/manual/toc/persons/',
+			sources: [
+				{
+					title: 'NASA',
+					link: 'https://www.nasa.gov/astronauts/biographies/fyodor-yurchikhin/biography'
+				}
+			],
+			shortDesc: 'lorem',
+			fullDesc: ['lorem']
+		}
 	]
 };
 
