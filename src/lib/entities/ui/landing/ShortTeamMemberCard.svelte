2457209c6f8da0d33933126888111b3b2caef25c
DarkDev <darkdev_brand@riseup.net>
2023-04-13 01:12:00 +0300

    upd text classes

--- src/lib/entities/ui/landing/ShortTeamMemberCard.svelte
+++ src/lib/entities/ui/landing/ShortTeamMemberCard.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div
-	class="mx-1 my-12 flex h-32 w-60  flex-col justify-center rounded-md border-2  border-violet-700 bg-gray-800  px-2 pb-4 text-center text-violet-500 shadow-md shadow-violet-700 xl:mx-6  "
+	class="mx-1 my-12 flex h-32 w-60  flex-col justify-center rounded-md border-2  border-violet-700 bg-violet-800  px-2 pb-4 text-center text-gray-900 shadow-md shadow-violet-700 xl:mx-6  "
 >
 	<img
 		alt={member.fullName}
@@ -11,7 +11,7 @@
 		src={member.img}
 	/>
 	<div class="flex-1">
-		<p class="text-xl font-semibold leading-snug">{member.fullName}</p>
-		<p>{member.function}</p>
+		<p class="md:text-3xl lg:text-3xl xl:text-3xl text-2xl  font-bold leading-snug">{member.fullName}</p>
+		<!-- <p class="font-semibold">{member.function}</p> -->
 	</div>
 </div>
