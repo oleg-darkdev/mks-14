a60df24ce20786a571b75236aa4c462a50fa474b
DarkDev <darkdev_brand@riseup.net>
2023-04-12 20:31:02 +0300

    fix extensions blocks

--- src/lib/shared/ui/landing/FooterLink.svelte
+++ src/lib/shared/ui/landing/FooterLink.svelte
@@ -4,6 +4,6 @@
 
 <li class='m-1'>
 	<a href="{link}" class="text-base font-normal text-gray-900 ">
-    <img src="{data.img}" class='object-cover rounded-lg h-20 w-40' alt={data.title}>
+    <img src="{data.img}" class='object-cover rounded-lg h-20 lg:w-40 md:w-40 xl:w-40 w-32' alt={data.title}>
 	</a>
 </li>
