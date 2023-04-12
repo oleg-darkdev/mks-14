944663ae6e21c9fd4c1d2937f7dba310ad3694f9
DarkDev <darkdev_brand@riseup.net>
2023-04-12 20:00:07 +0300

    set the z-30

--- src/lib/shared/ui/SmallLogoLink.svelte
+++ src/lib/shared/ui/SmallLogoLink.svelte
@@ -1,3 +1,3 @@
-<a rel="noopener noreferrer" href="/" aria-label="Go to homepage" class="flex items-center">
+<a rel="noopener noreferrer" href="/" aria-label="Go to homepage" class="z-30 flex items-center">
 	<img src="/images/logo.svg" alt="mks-14 logo" class="w-40" srcset="" />
 </a>
