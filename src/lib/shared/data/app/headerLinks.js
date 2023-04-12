4e0dbd22b09150601d490b608d6df610f33569f2
DarkDev <darkdev_brand@riseup.net>
2023-04-12 19:59:18 +0300

    upd text

--- src/lib/shared/data/app/headerLinks.js
+++ src/lib/shared/data/app/headerLinks.js
@@ -1,10 +1,11 @@
 const headerLinks = [
 	{ link: '/app', img: 'header_app.svg', text: 'App' },
-	{ link: '/app', img: 'header_app.svg', text: 'Locations' },
-	{ link: '/app', img: 'header_app.svg', text: 'Experiments' },
-	{ link: '/app', img: 'header_app.svg', text: 'Characters' },
-	{ link: '/app', img: 'header_app.svg', text: 'Extensions' },
-	{ link: '/app', img: 'header_app.svg', text: 'Synopsis' }
+  { link: '/app', img: 'header_extensions.svg', text: 'Extensions' },
+	{ link: '/app', img: 'header_locations.svg', text: 'Locations' },
+	{ link: '/app', img: 'header_experiments.svg', text: 'Experiments' },
+	{ link: '/app', img: 'header_сharacters.svg', text: 'Characters' },
+{ link: '/app', img: 'header_synopsis.svg', text: 'Synopsis' }
+	
 ];
 
 export default headerLinks;
