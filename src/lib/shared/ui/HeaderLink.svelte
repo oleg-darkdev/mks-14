9442701db98955b673e7c42a36e2abec5fde3903
DarkDev <darkdev_brand@riseup.net>
2023-04-12 20:29:57 +0300

    refactoring

--- src/lib/shared/ui/HeaderLink.svelte
+++ src/lib/shared/ui/HeaderLink.svelte
@@ -2,10 +2,9 @@
 	export let link;
 </script>
 
-	<img src="/images/header/{link.img}" class="h-10 w-10" />
-	<a
-		rel="noopener noreferrer"
-		href={link.link}
-		class="-mb-1 flex items-center border-b-2 border-transparent px-1 text-sm  hover:border-b-violet-900 hover:text-violet-900"
-		>{link.text}</a
-	>
+<a rel="noopener noreferrer" href={link.link} class="flex items-center">
+	<figure class="flex flex-col items-center px-1">
+		<img src="/images/header/{link.img}" class="h-12 w-auto" />
+		<figcaption class="text-center text-xs ">{link.text}</figcaption>
+	</figure>
+</a>
