74b9842ee951bb00786e52d4f3936a1aa17b3130
DarkDev <darkdev_brand@riseup.net>
2023-04-13 17:10:27 +0300

    style: remove empty lines

--- src/lib/widgets/ui/landing/Stats.svelte
+++ src/lib/widgets/ui/landing/Stats.svelte
@@ -2,39 +2,31 @@
 	import { StatElement } from '../../../shared/';
 
 	const stats = [
-		// {
-		// 	id: 'scifi',
-		// 	title: 'Hard Sci-fi',
-		// 	img: '/images/stats/stats_scifi.svg',
-    //   count: '+'
-		// },
 		{
 			id: 'astronauts',
 			title: 'Astronauts',
 			img: '/images/stats/stats_astronauts.svg',
-      count: 160
+			count: 160
 		},
 		{
 			id: 'experiments',
 			title: 'Experiments',
 			img: '/images/stats/stats_experiments.svg',
-      count: 480
+			count: 480
 		},
 		{
 			id: 'extensions',
 			title: 'Extensions',
 			img: '/images/stats/stats_extensions.svg',
-      count: "30+"
+			count: 30
 		}
 	];
 </script>
 
-<section class="bg-violet-700 p-6 text-gray-100">
-	<div class="grid grid-cols-1 gap-6 sm:grid-cols-1 xl:grid-cols-3 container mx-auto ">
+<section class="bg-violet-700 p-2 py-12 text-gray-100">
+	<div class="container mx-auto grid grid-cols-1 gap-6 sm:grid-cols-1 xl:grid-cols-3 ">
 		{#each stats as stat}
 			<StatElement {stat} />
 		{/each}
 	</div>
 </section>
-
-<!-- extensions -->
