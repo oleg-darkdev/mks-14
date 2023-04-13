7ba26fff87a6b47c77722209646d69bb7b21c328
DarkDev <darkdev_brand@riseup.net>
2023-04-13 17:03:43 +0300

    style: upscale btn`s size

--- src/lib/entities/ui/StepsBtns.svelte
+++ src/lib/entities/ui/StepsBtns.svelte
@@ -7,18 +7,10 @@
 
 <div class="flex w-full flex-row flex-wrap justify-center">
 	{#if step != 1}
-		<button
-			on:click={() => stepDecrease()}
-			class="m-2 inline-block rounded-lg border border-violet-700 bg-gray-900  py-2 px-4 font-medium text-violet-400 shadow-md duration-150 hover:bg-gray-100 hover:bg-gray-700 hover:text-gray-900 hover:shadow-none active:bg-gray-900"
+		<button on:click={() => stepDecrease()} class="btn-outline btn-primary btn-lg btn mr-2"
+			>Wracaj</button
 		>
-			Back
-		</button>
 	{/if}
 
-	<button
-		on:click={() => stepIncrease()}
-		class="m-2 inline-block rounded-lg bg-violet-700 py-2 px-4 font-medium text-white shadow-md duration-150 hover:bg-gray-100 hover:bg-gray-700 hover:text-gray-900 hover:shadow-none active:bg-gray-900"
-	>
-		Next
-	</button>
+	<button on:click={() => stepIncrease()} class="btn-primary btn-lg btn">Dalej</button>
 </div>
