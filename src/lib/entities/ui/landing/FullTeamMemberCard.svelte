14a061cfb034dfb552c5f000ac28d04296b76f6e
DarkDev <darkdev_brand@riseup.net>
2023-04-13 17:10:52 +0300

    alfa v.

--- src/lib/entities/ui/landing/FullTeamMemberCard.svelte
+++ src/lib/entities/ui/landing/FullTeamMemberCard.svelte
@@ -3,49 +3,21 @@
 </script>
 
 <div
-	class="xl:mx-6 lg:mx-6 md:mx-6 mx-1  mt-20 mb-4 flex lg:px-8 xl:px-8 md:px-6 px-2 w-full flex-col justify-center rounded-md border-2 border-violet-700 bg-violet-700 text-center text-gray-900  shadow-md shadow-violet-700 md:w-96 lg:w-80 xl:w-64"
+	class="mx-1 mt-20 mb-4 flex  w-full max-w-md flex-col justify-center rounded-md border-2 border-violet-700 bg-violet-700 px-2 text-center text-gray-900 shadow-md shadow-violet-700 md:mx-6 md:px-6 lg:mx-6  lg:px-8 xl:mx-6 xl:px-8"
 >
 	<img
 		alt={member.fullName}
-		class="-mt-20 h-40 w-40 flex-shrink-0 self-center rounded-lg bg-gray-500 bg-cover bg-center"
+		class="-mt-20 h-40 w-40 border-2 border-violet-700 flex-shrink-0 self-center rounded-lg bg-gray-500 bg-cover bg-center"
 		src={member.img}
 	/>
 	<div class="my-2 flex-1">
-		<p class="text-xl font-semibold leading-snug">{member.fullName}</p>
-		<p>{member.function}</p>
+		<p class="md:text-5xl lg:text-5xl xl:text-5xl text-4xl  font-black leading-snug">{member.fullName}</p>
+		<p class="md:text-3xl lg:text-3xl xl:text-3xl text-2xl font-bold">{member.function}</p>
 	</div>
 	<div class="flex items-center justify-center space-x-3 border-t-2 p-3">
-		<!-- <a rel="noopener noreferrer" href="#" title="Email" class="text-gray-900 hover:text-violet-400">
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				viewBox="0 0 20 20"
-				fill="currentColor"
-				class="h-5 w-5"
-			>
-				<path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
-				<path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
-			</svg>
-		</a> -->
-		<!-- <a
-			rel="noopener noreferrer"
-			href=""
-			title="Twitter"
-			class="text-gray-900 hover:text-violet-400"
-		>
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				viewBox="0 0 50 50"
-				fill="currentColor"
-				class="h-5 w-5"
-			>
-				<path
-					d="M 50.0625 10.4375 C 48.214844 11.257813 46.234375 11.808594 44.152344 12.058594 C 46.277344 10.785156 47.910156 8.769531 48.675781 6.371094 C 46.691406 7.546875 44.484375 8.402344 42.144531 8.863281 C 40.269531 6.863281 37.597656 5.617188 34.640625 5.617188 C 28.960938 5.617188 24.355469 10.21875 24.355469 15.898438 C 24.355469 16.703125 24.449219 17.488281 24.625 18.242188 C 16.078125 17.8125 8.503906 13.71875 3.429688 7.496094 C 2.542969 9.019531 2.039063 10.785156 2.039063 12.667969 C 2.039063 16.234375 3.851563 19.382813 6.613281 21.230469 C 4.925781 21.175781 3.339844 20.710938 1.953125 19.941406 C 1.953125 19.984375 1.953125 20.027344 1.953125 20.070313 C 1.953125 25.054688 5.5 29.207031 10.199219 30.15625 C 9.339844 30.390625 8.429688 30.515625 7.492188 30.515625 C 6.828125 30.515625 6.183594 30.453125 5.554688 30.328125 C 6.867188 34.410156 10.664063 37.390625 15.160156 37.472656 C 11.644531 40.230469 7.210938 41.871094 2.390625 41.871094 C 1.558594 41.871094 0.742188 41.824219 -0.0585938 41.726563 C 4.488281 44.648438 9.894531 46.347656 15.703125 46.347656 C 34.617188 46.347656 44.960938 30.679688 44.960938 17.09375 C 44.960938 16.648438 44.949219 16.199219 44.933594 15.761719 C 46.941406 14.3125 48.683594 12.5 50.0625 10.4375 Z"
-				/>
-			</svg>
-		</a> -->
 		<a
 			rel="noopener noreferrer"
-			href="{member.linkedin}"
+			href={member.linkedin}
 			title="LinkedIn"
 			class="text-gray-900 hover:text-violet-400"
 		>
@@ -60,9 +32,10 @@
 				/>
 			</svg>
 		</a>
+
 		<a
 			rel="noopener noreferrer"
-			href="{member.github}"
+			href={member.github}
 			title="GitHub"
 			class="text-gray-900 hover:text-violet-400"
 		>
