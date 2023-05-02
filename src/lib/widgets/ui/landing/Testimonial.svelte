d2425f340daeca26399453196b0a05d670c255c4
DarkDev <darkdev_brand@riseup.net>
2023-05-02 16:06:57 +0300

    clear lorem impsum data

--- src/lib/widgets/ui/landing/Testimonial.svelte
+++ src/lib/widgets/ui/landing/Testimonial.svelte
@@ -8,19 +8,17 @@
 		<p
 			class="hover:text-violet-700 px-6 py-2 text-center text-2xl font-semibold text-gray-300 sm:text-3xl sm:font-bold md:text-4xl lg:max-w-2xl xl:max-w-4xl"
 		>
-			"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
-			been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
-			galley of type and scrambled it to make a type specimen book."
+			"Najlepszy zaprezentowany prototyp, w historii hackathonu 'Science Game Jam'"
 		</p>
 		<div class="flex justify-center space-x-2">
 			<img
-				src="https://source.unsplash.com/80x80/?portrait?1"
-				alt=""
+				src="/images/testimotional.jpg"
+				alt="Avatar Cyrus Bukowsky"
 				class="h-20 w-20 rounded-md bg-cover bg-center bg-gray-500 bg-gray-700"
 			/>
 			<div class="text-3xl ">
-				<p class="leading-tight">Lorem impsum</p>
-				<p class="text-sm leading-tight text-gray-300">Founder, Company</p>
+				<p class="leading-tight">Cyrus Bukowsky</p>
+				<p class="text-sm leading-tight text-gray-300">Organizator hakatonu, Science Game Jam</p>
 			</div>
 		</div>
 	</div>
